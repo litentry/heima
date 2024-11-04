480018912357b9f3ff09f756f651f183798f921e
felixfaisal <faisalahmedfarooq46@gmail.com>
2024-11-04 09:58:28 +0000

    refactor: adjust log level for invalid connection hash

--- tee-worker/identity/core-primitives/top-pool/src/watcher.rs
+++ tee-worker/identity/core-primitives/top-pool/src/watcher.rs
@@ -124,7 +124,7 @@ where
 
 	fn send(&mut self, status: TrustedOperationStatus) {
 		if let Err(e) = self.rpc_response_sender.update_status_event(*self.hash(), status) {
-			error!("failed to send status update to rpc client: {:?}", e);
+			warn!("failed to send status update to rpc client: {:?}", e);
 		}
 	}
 
