4a4328254e9d4fa26b195d15cbe0d667d8d93762
Francisco Silva <francisco@kawagarbo-tech.io>
2024-11-07 17:29:58 +0000

    updating definitions

--- tee-worker/identity/client-api/parachain-api/prepare-build/interfaces/trusted_operations/definitions.ts
+++ tee-worker/identity/client-api/parachain-api/prepare-build/interfaces/trusted_operations/definitions.ts
@@ -7,6 +7,13 @@ export default {
                 get: "(Getter)",
             },
         },
+        TrustedOperationAuthenticated: {
+            _enum: {
+                indirect_call: "Null",
+                direct_call: "(TrustedCallAuthenticated)",
+                get: "(Getter)",
+            },
+        },
         TrustedCallSigned: {
             call: "TrustedCall",
             index: "u32",
@@ -25,7 +32,7 @@ export default {
         },
         TrustedCallAuthenticated: {
             call: "TrustedCall",
-            nonce: "Index",
+            nonce: "u32",
             authentication: "TCAuthentication",
         },
 
