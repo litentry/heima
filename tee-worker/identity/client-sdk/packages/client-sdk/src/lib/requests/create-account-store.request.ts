513a9129789ff4fc0b77285866718b97d7d1b1d6
Francisco Silva <francisco@kawagarbo-tech.io>
2024-11-14 16:27:39 +0000

    updating create-account-store to support email authentication

--- tee-worker/identity/client-sdk/packages/client-sdk/src/lib/requests/create-account-store.request.ts
+++ tee-worker/identity/client-sdk/packages/client-sdk/src/lib/requests/create-account-store.request.ts
@@ -16,7 +16,23 @@ import { createRequestType } from '../type-creators/request';
 import type { JsonRpcRequest } from '../util/types';
 
 /**
- * OmniAccount: Create the OmniAccount for the given Identity
+ * Creates an account store on the Litentry Parachain.
+ *
+ * @param {ApiPromise} api - Litentry Parachain API instance from Polkadot.js.
+ * @param {Object} data - The data required to create the account store.
+ * @param {LitentryIdentity} data.omniAccount - The user's OmniAccount. Use `createLitentryIdentityType` helper to create this struct.
+ * @param {LitentryIdentity} data.who - The user's account. Use `createLitentryIdentityType` helper to create this struct.
+ * @returns {Promise<Object>} - A promise that resolves to an object containing the payload to sign (if applicable) and a send function.
+ * @returns {string} [payloadToSign] - The payload to sign if who is not an email identity.
+ * @returns {Function} send - A function to send the request to the Enclave.
+ * @returns {Promise<Object>} send.args - The arguments required to send the request.
+ * @returns {string} send.args.authentication - The authentication string. If who is
+ * an email identity, this is the email verification code. If the who is not an email identity, this is the
+ * signed payload.
+ * @returns {Promise<Object>} send.response - The response from the Enclave.
+ * @returns {WorkerRpcReturnValue} send.response.response - The response value from the Enclave.
+ * @returns {string} send.response.blockHash - The block hash of the transaction.
+ * @returns {string} send.response.extrinsicHash - The extrinsic hash of the transaction.
  */
 export async function createAccountStore(
   /** Litentry Parachain API instance from Polkadot.js */
@@ -28,8 +44,8 @@ export async function createAccountStore(
     who: LitentryIdentity;
   }
 ): Promise<{
-  payloadToSign: string;
-  send: (args: { signedPayload: string }) => Promise<{
+  payloadToSign?: string;
+  send: (args: { authentication: string }) => Promise<{
     response: WorkerRpcReturnValue;
     blockHash: string;
     extrinsicHash: string;
@@ -51,15 +67,8 @@ export async function createAccountStore(
     omniAccount.asSubstrate.toHex()
   );
 
-  const payloadToSign = createPayloadToSign({
-    who,
-    call,
-    nonce,
-    shard: shardU8,
-  });
-
   const send = async (args: {
-    signedPayload: string;
+    authentication: string;
   }): Promise<{
     response: WorkerRpcReturnValue;
     blockHash: string;
@@ -69,7 +78,7 @@ export async function createAccountStore(
 
     const request = await createRequestType(api, {
       signer: who,
-      signature: args.signedPayload,
+      signature: args.authentication,
       call,
       nonce,
       shard: shardU8,
@@ -101,6 +110,17 @@ export async function createAccountStore(
       blockHash: block_hash.toString(),
     };
   };
+
+  if (who.isEmail) {
+    return { send };
+  }
+
+  const payloadToSign = createPayloadToSign({
+    who,
+    call,
+    nonce,
+    shard: shardU8,
+  });
 
   return {
     payloadToSign,
