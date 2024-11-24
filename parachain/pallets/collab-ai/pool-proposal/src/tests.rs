7f4c4a99341b119fa4b30e758840e75f35ee9d94
Minqi Wang <wangminqi@aliyun.com>
2024-11-24 19:03:54 +0800

    chore: add tests

--- parachain/pallets/collab-ai/pool-proposal/src/tests.rs
+++ parachain/pallets/collab-ai/pool-proposal/src/tests.rs
@@ -88,9 +88,8 @@ fn test_pre_stake_proposal_ok() {
 		let pool_last_time = 10000;
 		let estimated_pool_reward = 2_000_000_000_000_000_000_000u128;
 		let pool_info_hash: H256 = H256([2; 32]);
-		let _pool_info_hash_2: H256 = H256([3; 32]);
 
-		// Worked
+		// Setup
 		assert_ok!(PoolProposal::propose_investing_pool(
 			RuntimeOrigin::signed(user_a.clone()),
 			max_pool_size,
@@ -127,6 +126,12 @@ fn test_pre_stake_proposal_ok() {
 			500_000_000_000_000_000_000u128,
 		));
 
+		assert_events(vec![RuntimeEvent::PoolProposal(crate::Event::PoolPreInvested {
+			user: user_a.clone(),
+			pool_proposal_index: 1u128,
+			amount: 500_000_000_000_000_000_000u128,
+		})]);
+
 		// Go to proposal expire time
 		roll_to(150);
 		// Proposal Expired, Not work
@@ -140,3 +145,86 @@ fn test_pre_stake_proposal_ok() {
 		);
 	})
 }
+
+fn test_withdraw_pre_investing_ok() {
+	new_test_ext().execute_with(|| {
+		let user_a: AccountId32 = AccountId32::from([1u8; 32]);
+		let user_b: AccountId32 = AccountId32::from([2u8; 32]);
+		Balances::make_free_balance_be(&user_a, 100_000_000_000_000_000_000);
+		Balances::make_free_balance_be(&user_b, 100_000_000_000_000_000_000);
+
+		let max_pool_size = 10_000_000_000_000_000_000u128;
+		let proposal_last_time = 100;
+		let pool_last_time = 10000;
+		let estimated_pool_reward = 2_000_000_000_000_000_000_000u128;
+		let pool_info_hash: H256 = H256([2; 32]);
+
+		// Setup
+		assert_ok!(PoolProposal::propose_investing_pool(
+			RuntimeOrigin::signed(user_a.clone()),
+			max_pool_size,
+			proposal_last_time,
+			pool_last_time,
+			estimated_pool_reward,
+			pool_info_hash
+		));
+
+		// Normal pre staking worked
+		assert_ok!(PoolProposal::pre_stake_proposal(
+			RuntimeOrigin::signed(user_a.clone()),
+			1u128,
+			5_000_000_000_000_000_000u128,
+		));
+
+		// Can not withdraw if pool is fullfiled
+		assert_noop!(
+			PoolProposal::withdraw_pre_investing(
+				RuntimeOrigin::signed(user_a.clone()),
+				1u128,
+				1_000_000_000_000_000_000u128,
+			),
+			crate::Error::<Test>::ProposalPreInvestingLocked
+		);
+
+		// Fullfil the pool
+		assert_ok!(PoolProposal::pre_stake_proposal(
+			RuntimeOrigin::signed(user_a.clone()),
+			1u128,
+			495_000_000_000_000_000_000u128,
+		));
+
+		// pre investing and queued
+		assert_events(vec![RuntimeEvent::PoolProposal(crate::Event::PoolPreInvested {
+			user: user_a.clone(),
+			pool_proposal_index: 1u128,
+			amount: 5_000_000_000_000_000_000u128,
+		})]);
+		assert_events(vec![RuntimeEvent::PoolProposal(crate::Event::PoolPreStakeQueued {
+			user: user_a.clone(),
+			pool_proposal_index: 1u128,
+			amount: 490_000_000_000_000_000_000u128,
+		})]);
+
+		// Withdraw succeed
+		assert_ok!(PoolProposal::withdraw_pre_investing(
+			RuntimeOrigin::signed(user_a.clone()),
+			1u128,
+			490_000_000_000_000_000_000u128,
+		));
+		assert_events(vec![RuntimeEvent::PoolProposal(crate::Event::PoolWithdrawed {
+			user: user_a.clone(),
+			pool_proposal_index: 1u128,
+			amount: 490_000_000_000_000_000_000u128,
+		})]);
+	})
+}
+
+fn test_public_vote_proposal_ok() {}
+
+fn test_guardian_participate_proposal_ok() {}
+
+fn test_bake_proposal_ok() {}
+
+fn test_dissolve_proposal_ok() {}
+
+// TODO: solve pending test, types test
