a77b11842073f749abc394acb3972785f4a8a1f4
Francisco Silva <francisco@kawagarbo-tech.io>
2024-12-02 17:16:23 +0000

    adding unit test

--- tee-worker/identity/litentry/core/identity-verification/src/web2/google.rs
+++ tee-worker/identity/litentry/core/identity-verification/src/web2/google.rs
@@ -24,3 +24,23 @@ pub fn get_authorize_data(client_id: &str, redirect_uri: &str) -> AuthorizeData 
 
 	AuthorizeData { authorize_url: authorize_url.into(), state }
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use std::format;
+	use url::Url;
+
+	#[test]
+	fn test_get_authorize_data() {
+		let client_id = "client_id";
+		let redirect_uri = "http://localhost:8080";
+		let authorize_data = get_authorize_data(client_id, redirect_uri);
+
+		let authorize_url = Url::parse(&authorize_data.authorize_url).unwrap();
+		std::println!("{:?}", authorize_url.as_str());
+		let expected_url = format!("https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=client_id&redirect_uri=http%3A%2F%2Flocalhost%3A8080&scope=openid+email&state={}", authorize_data.state);
+
+		assert_eq!(authorize_url.as_str(), expected_url);
+	}
+}
