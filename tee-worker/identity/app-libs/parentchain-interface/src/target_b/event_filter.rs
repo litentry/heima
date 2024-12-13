44a762c27eb2b64fa03d447e04510cacc5827719
Kasper Ziemianek <kasper.ziemianek@gmail.com>
2024-12-13 18:48:01 +0100

    Remove `bitacross-worker` (#3203)
    * remove bitacross
    
    * remove macros
    
    ---------
    
    Co-authored-by: Kailai Wang <Kailai.Wang@hotmail.com>

--- tee-worker/identity/app-libs/parentchain-interface/src/target_b/event_filter.rs
+++ tee-worker/identity/app-libs/parentchain-interface/src/target_b/event_filter.rs
@@ -96,23 +96,11 @@ impl FilterEvents for FilterableEvents {
 		Ok(Vec::new())
 	}
 
-	fn get_relayer_added_events(&self) -> Result<Vec<RelayerAdded>, Self::Error> {
-		self.filter()
-	}
-
-	fn get_relayers_removed_events(&self) -> Result<Vec<RelayerRemoved>, Self::Error> {
-		self.filter()
-	}
-
 	fn get_enclave_added_events(&self) -> Result<Vec<EnclaveAdded>, Self::Error> {
 		self.filter()
 	}
 
 	fn get_enclave_removed_events(&self) -> Result<Vec<EnclaveRemoved>, Self::Error> {
-		self.filter()
-	}
-
-	fn get_btc_wallet_generated_events(&self) -> Result<Vec<BtcWalletGenerated>, Self::Error> {
 		self.filter()
 	}
 
