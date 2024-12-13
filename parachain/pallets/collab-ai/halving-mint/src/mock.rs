83d1ad23fee778c5007c4923dd8efa95c7953367
Kailai Wang <Kailai.Wang@hotmail.com>
2024-12-13 18:41:44 +0000

    move to collab-ai

--- parachain/pallets/collab-ai/halving-mint/src/mock.rs
+++ parachain/pallets/collab-ai/halving-mint/src/mock.rs
@@ -17,13 +17,12 @@
 use crate::{self as pallet_halving_mint, Config, Instance1, OnTokenMinted};
 use frame_support::pallet_prelude::*;
 use frame_support::traits::tokens::{fungibles::Mutate, Preservation};
-use frame_support::{construct_runtime, parameter_types, traits::AsEnsureOriginWithArg, PalletId};
-use frame_system::{EnsureRoot, EnsureSigned};
-use sp_core::{ConstU32, ConstU64, H256};
-use sp_runtime::{
-	traits::{BlakeTwo256, IdentityLookup},
-	BuildStorage,
+use frame_support::{
+	construct_runtime, derive_impl, parameter_types, traits::AsEnsureOriginWithArg, PalletId,
 };
+use frame_system::{EnsureRoot, EnsureSigned};
+use sp_core::{ConstU32, ConstU64};
+use sp_runtime::BuildStorage;
 
 type AccountId = u64;
 type Balance = u64;
@@ -39,34 +38,21 @@ construct_runtime!(
 	}
 );
 
-parameter_types! {
-	pub const BlockHashCount: u32 = 250;
+#[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
+impl frame_system::Config for Test {
+	type Block = frame_system::mocking::MockBlock<Test>;
+	type AccountData = pallet_balances::AccountData<Balance>;
 }
 
-impl frame_system::Config for Test {
-	type BaseCallFilter = frame_support::traits::Everything;
-	type BlockWeights = ();
-	type BlockLength = ();
-	type Block = frame_system::mocking::MockBlock<Test>;
-	type DbWeight = ();
-	type RuntimeOrigin = RuntimeOrigin;
-	type Nonce = u64;
-	type RuntimeCall = RuntimeCall;
-	type Hash = H256;
-	type Hashing = BlakeTwo256;
-	type AccountId = u64;
-	type Lookup = IdentityLookup<Self::AccountId>;
-	type RuntimeEvent = RuntimeEvent;
-	type BlockHashCount = BlockHashCount;
-	type Version = ();
-	type PalletInfo = PalletInfo;
-	type AccountData = pallet_balances::AccountData<u64>;
-	type OnNewAccount = ();
-	type OnKilledAccount = ();
-	type SystemWeightInfo = ();
-	type SS58Prefix = ();
-	type OnSetCode = ();
-	type MaxConsumers = frame_support::traits::ConstU32<16>;
+parameter_types! {
+	pub const ExistentialDeposit: Balance = 1;
+}
+
+#[derive_impl(pallet_balances::config_preludes::TestDefaultConfig)]
+impl pallet_balances::Config for Test {
+	type Balance = Balance;
+	type ExistentialDeposit = ExistentialDeposit;
+	type AccountStore = System;
 }
 
 parameter_types! {
@@ -99,27 +85,6 @@ impl pallet_assets::Config for Test {
 	type CallbackHandle = ();
 	#[cfg(feature = "runtime-benchmarks")]
 	type BenchmarkHelper = ();
-}
-
-parameter_types! {
-	pub const ExistentialDeposit: u64 = 2;
-	pub const MaxLocks: u32 = 10;
-}
-
-impl pallet_balances::Config for Test {
-	type MaxLocks = ();
-	type Balance = u64;
-	type DustRemoval = ();
-	type RuntimeEvent = RuntimeEvent;
-	type ExistentialDeposit = ExistentialDeposit;
-	type AccountStore = System;
-	type WeightInfo = ();
-	type MaxReserves = ();
-	type ReserveIdentifier = ();
-	type FreezeIdentifier = ();
-	type MaxHolds = ();
-	type MaxFreezes = ();
-	type RuntimeHoldReason = ();
 }
 
 parameter_types! {
