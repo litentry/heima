95907f8eb09e6a288ca7d631f92d59f19cd85045
Kailai Wang <Kailai.Wang@hotmail.com>
2024-12-15 14:44:42 +0000

    fix tests

--- parachain/pallets/parachain-staking/src/tests.rs
+++ parachain/pallets/parachain-staking/src/tests.rs
@@ -222,12 +222,12 @@ fn cannot_set_collator_commission_to_current_collator_commission() {
 #[test]
 fn set_blocks_per_round_event_emits_correctly() {
 	ExtBuilder::default().build().execute_with(|| {
-		assert_ok!(ParachainStaking::set_blocks_per_round(RuntimeOrigin::root(), 6u32));
+		assert_ok!(ParachainStaking::set_blocks_per_round(RuntimeOrigin::root(), 12u32));
 		assert_last_event!(MetaEvent::ParachainStaking(Event::BlocksPerRoundSet {
 			current_round: 1,
 			first_block: 0,
 			old: 5,
-			new: 6,
+			new: 12,
 			new_per_round_inflation_min: Perbill::from_parts(926),
 			new_per_round_inflation_ideal: Perbill::from_parts(926),
 			new_per_round_inflation_max: Perbill::from_parts(926),
@@ -424,9 +424,9 @@ fn set_inflation_event_emits_correctly() {
 			annual_min: min,
 			annual_ideal: ideal,
 			annual_max: max,
-			round_min: Perbill::from_parts(57),
-			round_ideal: Perbill::from_parts(75),
-			round_max: Perbill::from_parts(93),
+			round_min: Perbill::from_parts(29),
+			round_ideal: Perbill::from_parts(38),
+			round_max: Perbill::from_parts(47),
 		}));
 	});
 }
@@ -460,9 +460,9 @@ fn set_inflation_storage_updates_correctly() {
 		assert_eq!(
 			ParachainStaking::inflation_config().round,
 			Range {
-				min: Perbill::from_parts(57),
-				ideal: Perbill::from_parts(75),
-				max: Perbill::from_parts(93)
+				min: Perbill::from_parts(29),
+				ideal: Perbill::from_parts(38),
+				max: Perbill::from_parts(47)
 			}
 		);
 	});
