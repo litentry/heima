b0604c57656390eb8ace3fce9e644ad136c4d863
Francisco J Silva <franjs.francisco@gmail.com>
2025-01-15 18:13:42 +0000

    adding submit_native_request method (WIP)

--- tee-worker/omni-executor/rpc-server/src/methods/mod.rs
+++ tee-worker/omni-executor/rpc-server/src/methods/mod.rs
@@ -1,9 +1,12 @@
 mod get_shielding_key;
+mod submit_native_request;
 
 use crate::server::RpcContext;
 use get_shielding_key::register_get_shielding_key;
 use jsonrpsee::RpcModule;
+use submit_native_request::register_submit_native_request;
 
 pub fn register_methods(module: &mut RpcModule<RpcContext>) {
 	register_get_shielding_key(module);
+	register_submit_native_request(module);
 }
