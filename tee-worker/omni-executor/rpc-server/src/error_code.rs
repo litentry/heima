37aca5a8a2de31cb989183b220696f86e554ea9c
Francisco J Silva <franjs.francisco@gmail.com>
2025-01-16 16:32:21 +0000

    error_code initial setup

---
+++ tee-worker/omni-executor/rpc-server/src/error_code.rs
@@ -0,0 +1,8 @@
+// we should use -32000 to -32099 for implementation defined error codes,
+// see https://www.jsonrpc.org/specification#error_object
+
+pub const INVALID_AES_REQUEST_CODE: i32 = -32000;
+pub const INVALID_MRENCLAVE_CODE: i32 = -32001;
+pub const REQUEST_DECRYPTION_FAILED_CODE: i32 = -32002;
+pub const INVALID_AUTHENTICATED_CALL_CODE: i32 = -32003;
+pub const AUTHENTICATION_FAILED_CODE: i32 = -32004;
--- tee-worker/omni-executor/rpc-server/src/lib.rs
+++ tee-worker/omni-executor/rpc-server/src/lib.rs
@@ -1,3 +1,4 @@
+mod error_code;
 mod methods;
 mod server;
 mod shielding_key;
