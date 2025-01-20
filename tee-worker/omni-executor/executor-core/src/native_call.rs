4a2bfd6ebaf04ae73359948dc2b7af81b1dcf31f
Francisco J Silva <franjs.francisco@gmail.com>
2025-01-20 12:13:45 +0000

    moving native_call to executor-core

---
+++ tee-worker/omni-executor/executor-core/src/native_call.rs
@@ -0,0 +1,22 @@
+use parentchain_primitives::Identity;
+use parity_scale_codec::{Decode, Encode};
+
+#[derive(Encode, Decode, Clone, Debug, PartialEq, Eq)]
+#[allow(non_camel_case_types)]
+pub enum NativeCall {
+	// TODO: define the actual calls
+	noop(Identity),
+}
+
+impl NativeCall {
+	pub fn sender_identity(&self) -> &Identity {
+		match self {
+			NativeCall::noop(sender_identity) => sender_identity,
+		}
+	}
+
+	pub fn signature_message_prefix(&self) -> String {
+		// TODO: update this when adding request_batch_vc variant
+		"Token: ".to_string()
+	}
+}
