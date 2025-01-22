171b9bdc2ab303c535495246d5f0c2307d4fee29
Francisco J Silva <franjs.francisco@gmail.com>
2025-01-22 15:25:59 +0000

    refactoring mailer exports

--- tee-worker/omni-executor/heima/identity-verification/src/web2/email/mod.rs
+++ tee-worker/omni-executor/heima/identity-verification/src/web2/email/mod.rs
@@ -1,21 +1,19 @@
 mod mailer;
-
-use crate::helpers;
-use mailer::{template, Error, Mail, MailerTrait};
+pub use mailer::Mailer;
 
 pub fn generate_verification_code() -> String {
-	helpers::generate_otp(6)
+	crate::helpers::generate_otp(6)
 }
 
 pub async fn send_verification_email(
-	mailer: &impl MailerTrait,
+	mailer: &impl mailer::MailerTrait,
 	to: String,
 	verification_code: String,
-) -> Result<(), Error> {
-	let mail = Mail {
+) -> Result<(), mailer::Error> {
+	let mail = mailer::Mail {
 		to,
 		subject: "Verify your email".to_string(),
-		body: template::EMAIL_VERIFICATION_TEMPLATE
+		body: mailer::template::EMAIL_VERIFICATION_TEMPLATE
 			.replace("{{ verification_code }}", &verification_code),
 		content_type: "text/html".to_string(),
 	};
