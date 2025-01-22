394e827f5d868d09b1c346e4c90b91d1710e582d
Francisco J Silva <franjs.francisco@gmail.com>
2025-01-22 15:27:31 +0000

    adding request_email_verification_code method

--- tee-worker/omni-executor/rpc-server/src/methods/mod.rs
+++ tee-worker/omni-executor/rpc-server/src/methods/mod.rs
@@ -1,12 +1,15 @@
 mod get_shielding_key;
+mod request_email_verification_code;
 mod submit_aes_request;
 
 use crate::server::RpcContext;
 use get_shielding_key::register_get_shielding_key;
 use jsonrpsee::RpcModule;
+use request_email_verification_code::register_request_email_verification_code;
 use submit_aes_request::register_submit_aes_request;
 
 pub fn register_methods(module: &mut RpcModule<RpcContext>) {
 	register_get_shielding_key(module);
 	register_submit_aes_request(module);
+	register_request_email_verification_code(module);
 }
---
+++ tee-worker/omni-executor/rpc-server/src/methods/request_email_verification_code.rs
@@ -0,0 +1,37 @@
+use crate::server::RpcContext;
+use executor_core::storage::Storage;
+use heima_identity_verification::web2::email::{
+	generate_verification_code, send_verification_email,
+};
+use jsonrpsee::{
+	types::{ErrorCode, ErrorObject},
+	RpcModule,
+};
+use primitives::{Identity, Web2IdentityType};
+use storage::VerificationCodeStorage;
+
+pub fn register_request_email_verification_code(module: &mut RpcModule<RpcContext>) {
+	module
+		.register_async_method("omni_requestEmailVerificationCode", |params, ctx, _| async move {
+			let Ok(email) = params.one::<String>() else {
+				return Err(ErrorCode::ParseError.into());
+			};
+			let email_identity = Identity::from_web2_account(&email, Web2IdentityType::Email);
+			let verification_code_storage = VerificationCodeStorage::new(ctx.storage_db.clone());
+			let verification_code = generate_verification_code();
+
+			verification_code_storage
+				.insert(email_identity.hash(), verification_code.clone())
+				.map_err(|_| ErrorCode::InternalError)?;
+
+			send_verification_email(&ctx.mailer, email, verification_code)
+				.await
+				.map_err(|_| {
+					log::error!("Failed to send verification email");
+					ErrorCode::InternalError
+				})?;
+
+			Ok::<(), ErrorObject>(())
+		})
+		.expect("Failed to register omni_requestEmailVerificationCode method");
+}
