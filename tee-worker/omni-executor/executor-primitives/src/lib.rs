c129077825aab0e86cc31696f47ef3b1419807ba
Francisco J Silva <franjs.francisco@gmail.com>
2025-02-05 11:21:27 +0000

    exporting more types from executor-primitives

--- tee-worker/omni-executor/executor-primitives/src/lib.rs
+++ tee-worker/omni-executor/executor-primitives/src/lib.rs
@@ -15,7 +15,10 @@
 // along with Litentry.  If not, see <https://www.gnu.org/licenses/>.
 
 mod validation_data;
-pub use validation_data::{ValidationData, Web2ValidationData, Web3ValidationData};
+pub use validation_data::{
+	DiscordValidationData, TwitterValidationData, ValidationData, Web2ValidationData,
+	Web3ValidationData,
+};
 
 pub mod signature;
 pub mod utils;
