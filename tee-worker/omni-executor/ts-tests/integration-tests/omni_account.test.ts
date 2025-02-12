0463805b7e71f4e233b450c26af0c03c1518b95d
Francisco J Silva <franjs.francisco@gmail.com>
2025-02-12 11:00:48 +0000

    adding test for publicize_account call

--- tee-worker/omni-executor/ts-tests/integration-tests/omni_account.test.ts
+++ tee-worker/omni-executor/ts-tests/integration-tests/omni_account.test.ts
@@ -98,4 +98,40 @@ describe('OmniAccount', function () {
         const memberAccount2: CorePrimitivesOmniAccountMemberAccount = accountStore.unwrap()[1];
         assert.isTrue(memberAccount2.isPrivate);
     });
+
+    step('test publicize_account', async function () {
+        const currentNonce = 1;
+        const bob = context.web3Wallets['substrate']['Bob'] as SubstrateSigner;
+        const bobIdentity = await bob.getIdentity(context.api);
+        const nativeCall = createNativeCall(
+            context.api,
+            ['publicize_account', '(LitentryIdentity, LitentryIdentity)'],
+            [aliceIdentity, bobIdentity]
+        );
+        const nativeCallAuthenticated = await createNativeCallAuthenticated(
+            context.api,
+            nativeCall,
+            aliceWallet,
+            context.api.createType('Index', currentNonce),
+            context.mrEnclave
+        );
+        await sendPlainRequestFromNativeCall(context, nativeCallAuthenticated);
+        const accountStore = await context.api.query.omniAccount.accountStore(omniAccount);
+        const membersCount = accountStore.unwrap().length;
+        assert.equal(membersCount, 2, 'account store members count should be 2');
+        const memberAccount1: CorePrimitivesOmniAccountMemberAccount = accountStore.unwrap()[0];
+        assert.equal(
+            memberAccount1.asPublic.asSubstrate.toHex(),
+            aliceIdentity.asSubstrate.toHex(),
+            'account store member 1 is not the expected member'
+        );
+        const memberAccount2: CorePrimitivesOmniAccountMemberAccount = accountStore.unwrap()[1];
+        assert.isTrue(memberAccount2.isPublic);
+        assert.equal(
+            memberAccount2.asPublic.asSubstrate.toHex(),
+            bobIdentity.asSubstrate.toHex(),
+            'account store member 2 is not the expected member'
+        );
+    });
+
 });
