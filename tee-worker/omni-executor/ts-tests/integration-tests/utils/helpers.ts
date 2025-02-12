64d53a83350f29963859fce4815d1d96f2bfdca4
Francisco J Silva <franjs.francisco@gmail.com>
2025-02-12 10:53:42 +0000

    adding helper to fund accounts

--- tee-worker/omni-executor/ts-tests/integration-tests/utils/helpers.ts
+++ tee-worker/omni-executor/ts-tests/integration-tests/utils/helpers.ts
@@ -1,6 +1,22 @@
 import { hexToU8a, compactStripLength, u8aToString } from '@polkadot/util';
 import { HexString } from '@polkadot/util/types';
+import { ApiPromise, Keyring } from 'parachain-api';
 
 export function decodeRpcBytesAsString(value: HexString): string {
     return u8aToString(compactStripLength(hexToU8a(value))[1]);
+}
+
+export async function fundAccount(api: ApiPromise, account: string, amount: bigint) {
+    console.log(`Funding account ${account} with ${amount}`);
+    const keyring = new Keyring({ type: 'sr25519' });
+    const alice = keyring.addFromUri('//Alice');
+    const transfer = api.tx.balances.transferAllowDeath(account, amount);
+
+    return new Promise<void>((resolve, reject) => {
+        transfer
+            .signAndSend(alice, ({ isFinalized }) => {
+                if (isFinalized) resolve();
+            })
+            .catch(reject);
+    });
 }
