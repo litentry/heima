7ac82003bef56871c889fbc93d52ec84d4d44ab3
Kai <7630809+Kailai-Wang@users.noreply.github.com>
2025-02-14 23:59:18 +0100

    Adjust weight consumption in parachain-staking (#3258)
    * reduce delegator count
    
    * use default pallet weights

--- parachain/runtime/paseo/src/lib.rs
+++ parachain/runtime/paseo/src/lib.rs
@@ -1053,9 +1053,9 @@ impl pallet_parachain_staking::Config for Runtime {
 	/// Minimum collators selected per round, default at genesis and minimum forever after
 	type MinSelectedCandidates = ConstU32<1>;
 	/// Maximum top delegations per candidate
-	type MaxTopDelegationsPerCandidate = ConstU32<1000>;
+	type MaxTopDelegationsPerCandidate = ConstU32<300>;
 	/// Maximum bottom delegations per candidate
-	type MaxBottomDelegationsPerCandidate = ConstU32<200>;
+	type MaxBottomDelegationsPerCandidate = ConstU32<100>;
 	/// Maximum delegations per delegator
 	type MaxDelegationsPerDelegator = ConstU32<100>;
 	type DefaultCollatorCommission = DefaultCollatorCommission;
@@ -1070,7 +1070,7 @@ impl pallet_parachain_staking::Config for Runtime {
 	type MinDelegatorStk = MinDelegatorStk;
 	type OnCollatorPayout = ();
 	type OnNewRound = ();
-	type WeightInfo = weights::pallet_parachain_staking::WeightInfo<Runtime>;
+	type WeightInfo = ();
 	type IssuanceAdapter = AssetsHandler;
 	type OnAllDelegationRemoved = ScoreStaking;
 }
