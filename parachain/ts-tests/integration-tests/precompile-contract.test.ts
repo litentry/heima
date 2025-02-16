bf72a931d0bd82aed460dbfd1836f5f55460bd89
Verin1005 <daqingchong0809@gmail.com>
2025-02-16 02:26:13 +0000

    fix event data parsing

--- parachain/ts-tests/integration-tests/precompile-contract.test.ts
+++ parachain/ts-tests/integration-tests/precompile-contract.test.ts
@@ -195,16 +195,15 @@ describeLitentry('Test Parachain Precompile Contract', ``, (context) => {
 
         expect(events.length).to.eq(1);
         const event_data = events[0].toHuman().data! as Array<string>;
-        console.log(`Print Event data: ${event_data}`);
+        console.log(`Print Event data: ${JSON.stringify(event_data)}`);
 
         // PaidIn(source_account, nonce, asset, resource_id, dest_chain, dest_account, amount)
-        // TODO: This is not working, objective undefined can not be compared
-        // expect(JSON.stringify(event_data[4])).to.eq(JSON.stringify({Ethereum:0}));
-        // expect(event_data[5]).to.eq(dest_address);
+        expect(JSON.stringify(event_data[4])).to.eq(JSON.stringify({ethereum:0}));
+        expect(event_data[5]).to.eq(dest_address);
 
         // 0.01 - 0.001 = 0.009
-        // const expectedBalance = bn1e18.div(bn100).sub(bn1e18.div(bn1000));
-        // expect(event_data[6].toString().replace(/,/g, '')).to.eq(expectedBalance.toString());
+        const expectedBalance = bn1e18.div(bn100).sub(bn1e18.div(bn1000));
+        expect(event_data[6].toString().replace(/,/g, '')).to.eq(expectedBalance.toString());
 
         console.timeEnd('Test precompile omni bridge contract');
     });
