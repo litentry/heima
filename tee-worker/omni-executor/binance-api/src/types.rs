5fbd368ea687f3604099e023ee14c132b59369ce
Francisco J Silva <franjs.francisco@gmail.com>
2025-03-04 16:23:45 +0000

    feat(binance-api): add AssetSymbol type and AssetInfo struct

--- tee-worker/omni-executor/binance-api/src/types.rs
+++ tee-worker/omni-executor/binance-api/src/types.rs
@@ -1,13 +1,22 @@
 use serde::{Deserialize, Serialize};
 
+pub type AssetSymbol = String;
+
 /// Convertible token pair
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct TokenPair {
-	pub from_asset: String,
-	pub to_asset: String,
+	pub from_asset: AssetSymbol,
+	pub to_asset: AssetSymbol,
 	pub from_asset_min_amount: String,
 	pub from_asset_max_amount: String,
 	pub to_asset_min_amount: String,
 	pub to_asset_max_amount: String,
+}
+
+/// Asset’s precision information
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AssetInfo {
+	pub asset: AssetSymbol,
+	pub fraction: u32,
 }
