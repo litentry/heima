89f43576430085737fd2a860b3e61712757eea78
Kailai-Wang <Kailai.Wang@hotmail.com>
2025-03-06 00:16:01 +0000

    Make hash enabled by default

--- parachain/runtime/paseo/build.rs
+++ parachain/runtime/paseo/build.rs
@@ -14,16 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Litentry.  If not, see <https://www.gnu.org/licenses/>.
 
-#[cfg(all(feature = "std", not(feature = "metadata-hash")))]
-fn main() {
-	substrate_wasm_builder::WasmBuilder::new()
-		.with_current_project()
-		.export_heap_base()
-		.import_memory()
-		.build();
-}
-
-#[cfg(all(feature = "std", feature = "metadata-hash"))]
+#[cfg(feature = "std")]
 fn main() {
 	substrate_wasm_builder::WasmBuilder::new()
 		.with_current_project()
