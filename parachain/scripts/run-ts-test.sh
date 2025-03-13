38aa1421b93478fc2b4e5434317af1dc1c84a867
Kasper Ziemianek <kasper.ziemianek@gmail.com>
2025-03-13 11:10:57 +0100

    remove old bridge scripts (#3287)
    Co-authored-by: Kai <7630809+Kailai-Wang@users.noreply.github.com>

--- parachain/scripts/run-ts-test.sh
+++ parachain/scripts/run-ts-test.sh
@@ -17,9 +17,5 @@ LITENTRY_PARACHAIN_DIR=${LITENTRY_PARACHAIN_DIR:-"/tmp/parachain_dev"}
 pnpm install
 pnpm run test-filter 2>&1 | tee -a "$LITENTRY_PARACHAIN_DIR/parachain_ci_test.log"
 
-# comment out for now - we use omni-bridge now
-# $ROOTDIR/parachain/scripts/launch-bridge.sh
-# pnpm run test-bridge 2>&1 | tee -a "$LITENTRY_PARACHAIN_DIR/parachain_ci_test.log"
-
 pnpm run test-evm-contract 2>&1 | tee -a "$LITENTRY_PARACHAIN_DIR/parachain_ci_test.log"
 pnpm run test-precompile-contract 2>&1 | tee -a "$LITENTRY_PARACHAIN_DIR/parachain_ci_test.log"
