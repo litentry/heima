564e9da3baa630faa6c48346b43f1741c8c54287
Faisal Ahmed <faisalahmedfarooq46@gmail.com>
2025-03-17 13:12:28 +0530

    fix: perform happy test and update the sol program

--- solana/accounting-contract/programs/accounting-contract/src/lib.rs
+++ solana/accounting-contract/programs/accounting-contract/src/lib.rs
@@ -1,5 +1,6 @@
 use anchor_lang::prelude::*;
 use anchor_lang::system_program;
+use serde::{Deserialize, Serialize};
 
 declare_id!("31weKQJQA9ZFYaVdnjtAXUoEGPW8UUFC5TEvgPJugAub");
 
@@ -7,22 +8,17 @@ declare_id!("31weKQJQA9ZFYaVdnjtAXUoEGPW8UUFC5TEvgPJugAub");
 pub mod accounting_contract {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
-
     pub fn set_admin(ctx: Context<SetAdmin>, new_admin: Pubkey) -> Result<()> {
-        let upgrade_authority = ctx
-            .accounts
-            .program_account
-            .upgrade_authority_address
-            .unwrap();
+        let upgrade_authority = match ctx.accounts.program_account.upgrade_authority_address {
+            Some(address) => address,
+            None => return Err(ErrorCode::UpgradeAuthorityNotFound.into()),
+        };
         require!(
             ctx.accounts.caller.key() == upgrade_authority,
             ErrorCode::Unauthorized
         );
-        ctx.accounts.admin_account.admin = new_admin;
+        let admin = &mut ctx.accounts.admin_account.admin;
+        *admin = new_admin;
         Ok(())
     }
 
@@ -49,10 +45,35 @@ pub mod accounting_contract {
         Ok(())
     }
 
+    pub fn withdraw_funds(ctx: Context<WithdrawFunds>, amount: u64) -> Result<()> {
+        require!(
+            ctx.accounts.admin_account.admin == ctx.accounts.signer.key(),
+            ErrorCode::Unauthorized
+        );
+
+        require!(
+            ctx.accounts.treasury.to_account_info().lamports() >= amount,
+            ErrorCode::OutOfBalance
+        );
+
+        **ctx
+            .accounts
+            .treasury
+            .to_account_info()
+            .try_borrow_mut_lamports()? -= amount;
+        **ctx
+            .accounts
+            .beneficiary
+            .to_account_info()
+            .try_borrow_mut_lamports()? += amount;
+
+        Ok(())
+    }
+
     pub fn create_pay_request(
         ctx: Context<CreatePayRequest>,
-        amount: u64,
         nonce: u64,
+        amount: u64,
     ) -> Result<()> {
         require!(
             ctx.accounts.worker.worker == ctx.accounts.signer.key(),
@@ -60,61 +81,75 @@ pub mod accounting_contract {
         );
 
         require!(
-            ctx.accounts.treasury.balance >= amount,
+            ctx.accounts.treasury.to_account_info().lamports() >= amount,
             ErrorCode::OutOfBalance
         );
 
-        system_program::transfer(
-            CpiContext::new(
-                ctx.accounts.system_program.to_account_info(),
-                system_program::Transfer {
-                    from: ctx.accounts.treasury.to_account_info(),
-                    to: ctx.accounts.beneficiary.to_account_info(),
-                },
-            ),
-            amount,
-        )?;
+        require!(
+            ctx.accounts.account_nonce.nonce < nonce,
+            ErrorCode::InvalidNonce
+        );
+
+        **ctx
+            .accounts
+            .treasury
+            .to_account_info()
+            .try_borrow_mut_lamports()? -= amount;
+        **ctx
+            .accounts
+            .beneficiary
+            .to_account_info()
+            .try_borrow_mut_lamports()? += amount;
 
         let payout_request = &mut ctx.accounts.pay_out_request;
         payout_request.beneficiary = *ctx.accounts.beneficiary.key;
         payout_request.amount = amount;
         payout_request.nonce = nonce;
 
+        let nonce = &mut ctx.accounts.account_nonce.nonce;
+        *nonce += 1;
+
         Ok(())
     }
 }
 
 #[account]
-#[derive(InitSpace)]
+#[derive(InitSpace, Serialize, Deserialize, Debug)]
 pub struct AdminAccount {
     pub admin: Pubkey,
 }
 
 #[account]
-#[derive(InitSpace)]
+#[derive(InitSpace, Serialize, Deserialize, Debug)]
 pub struct WorkerAccount {
     pub worker: Pubkey,
 }
 
 #[account]
-#[derive(InitSpace)]
-pub struct TreasuryAccount {
-    pub balance: u64,
-}
+#[derive(InitSpace, Serialize, Deserialize, Debug)]
+pub struct TreasuryAccount {}
 
 #[account]
 #[derive(InitSpace)]
 pub struct PayoutRequest {
     pub beneficiary: Pubkey,
+    pub nonce: u64,
     pub amount: u64,
+}
+
+#[account]
+#[derive(InitSpace, Serialize, Deserialize, Debug)]
+pub struct Nonce {
     pub nonce: u64,
 }
 
 #[derive(Accounts)]
 #[instruction(nonce: u64)]
 pub struct CreatePayRequest<'info> {
-    #[account(init_if_needed, payer = signer, space = 8 + PayoutRequest::INIT_SPACE, seeds = [beneficiary.to_account_info().key().to_bytes().as_ref(), &nonce.to_be_bytes(), b"payout_request"], bump)]
+    #[account(init, payer = signer, space = 8 + PayoutRequest::INIT_SPACE, seeds = [beneficiary.to_account_info().key().to_bytes().as_ref(), nonce.to_le_bytes().as_ref(), b"payout_request"], bump)]
     pub pay_out_request: Account<'info, PayoutRequest>,
+    #[account(init_if_needed, payer = signer, space = 8 + Nonce::INIT_SPACE, seeds = [beneficiary.to_account_info().key().to_bytes().as_ref(), b"nonce"], bump)]
+    pub account_nonce: Account<'info, Nonce>,
     #[account(mut)]
     pub signer: Signer<'info>,
     #[account(seeds = [b"worker"], bump)]
@@ -132,6 +167,18 @@ pub struct DepositFunds<'info> {
     pub treasury: Account<'info, TreasuryAccount>,
     #[account(mut)]
     pub depositor: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct WithdrawFunds<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(mut, seeds = [b"admin"], bump)]
+    pub admin_account: Account<'info, AdminAccount>,
+    #[account(mut, seeds = [b"treasury"], bump)]
+    pub treasury: Account<'info, TreasuryAccount>,
+    pub beneficiary: SystemAccount<'info>,
     pub system_program: Program<'info, System>,
 }
 
@@ -156,9 +203,6 @@ pub struct SetAdmin<'info> {
     pub system_program: Program<'info, System>,
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
-
 // ✅ Error handling
 #[error_code]
 pub enum ErrorCode {
@@ -170,4 +214,6 @@ pub enum ErrorCode {
     UpgradeAuthorityNotFound,
     #[msg("The program has run out of Sol tokens")]
     OutOfBalance,
+    #[msg("The nonce is invalid")]
+    InvalidNonce,
 }
--- solana/accounting-contract/tests/src/test_initialize.rs
+++ solana/accounting-contract/tests/src/test_initialize.rs
@@ -11,16 +11,31 @@ use anchor_client::{
     Client, Cluster, Program,
 };
 
-
 #[test]
-fn test_set_admin() {
+fn test_full_workerflow() {
     let program_id = "31weKQJQA9ZFYaVdnjtAXUoEGPW8UUFC5TEvgPJugAub";
-    let payer = Keypair::new();
+    // PK - 8QQds7P14EL1ZFjPLsTg2AHZaQHfNGH1EDW8wMhJmxaX
+    let payer = read_keypair_file("../test-account.json").expect("Failed to read keypair file");
+    // let payer = Keypair::new();
 
     let client = Client::new_with_options(Cluster::Localnet, &payer, CommitmentConfig::confirmed());
+
+    // Ensure the payer has tokens
     let program_id = Pubkey::from_str(program_id).unwrap();
     let program = client.program(program_id).unwrap();
     let program_pubkey = program.id();
+
+    let balance = program
+        .rpc()
+        .get_balance(&payer.pubkey())
+        .expect("Failed to fetch balance");
+
+    if balance == 0 {
+        program
+            .rpc()
+            .request_airdrop(&payer.pubkey(), 1_000_000_000)
+            .expect("Failed to airdrop SOL to payer");
+    }
 
     let account = program.rpc().get_account(&program_pubkey).unwrap();
     if account.owner != BPF_LOADER_UPGRADEABLE_ID {
@@ -41,13 +56,13 @@ fn test_set_admin() {
 
     let tx = program
         .request()
-        .accounts(first::accounts::SetAdmin {
-            admin_account: Pubkey::find_program_address(&[b"admin"], &program_id).0,
+        .accounts(accounting_contract::accounts::SetAdmin {
+            admin_account: Pubkey::find_program_address(&[b"admin"], &program.id()).0,
             caller: payer.pubkey(),
             program_account: address,
             system_program: anchor_client::solana_sdk::system_program::ID,
         })
-        .args(first::instruction::SetAdmin {
+        .args(accounting_contract::instruction::SetAdmin {
             new_admin: payer.pubkey(),
         })
         .send()
@@ -55,17 +70,100 @@ fn test_set_admin() {
 
     let tx = program
         .request()
-        .accounts(first::accounts::SetWorker {
-            worker_account: Pubkey::find_program_address(&[b"worker"], &program_id).0,
+        .accounts(accounting_contract::accounts::SetWorker {
+            worker_account: Pubkey::find_program_address(&[b"worker"], &program.id()).0,
             caller: payer.pubkey(),
-            admin_account: Pubkey::find_program_address(&[b"admin"], &program_id).0,
+            admin_account: Pubkey::find_program_address(&[b"admin"], &program.id()).0,
             system_program: anchor_client::solana_sdk::system_program::ID,
         })
-        .args(first::instruction::SetWorker {
+        .args(accounting_contract::instruction::SetWorker {
             new_worker: payer.pubkey(),
         })
         .send()
         .expect("Failed to send transaction");
 
-    println!("Your transaction signature: {}", tx);
+    let account_admin = program
+        .rpc()
+        .get_account(&Pubkey::find_program_address(&[b"admin"], &program.id()).0)
+        .unwrap();
+    let admin_struct: accounting_contract::AdminAccount =
+        bincode::deserialize(&account_admin.data[8..]).unwrap();
+
+    let payer_account = program.rpc().get_account(&payer.pubkey()).unwrap();
+
+    assert_eq!(payer.pubkey(), admin_struct.admin);
+
+    let account_worker = program
+        .rpc()
+        .get_account(&Pubkey::find_program_address(&[b"worker"], &program.id()).0)
+        .unwrap();
+    let worker_struct: accounting_contract::WorkerAccount =
+        bincode::deserialize(&account_worker.data[8..]).unwrap();
+
+    assert_eq!(payer.pubkey(), worker_struct.worker);
+
+    let tx = program
+        .request()
+        .accounts(accounting_contract::accounts::DepositFunds {
+            treasury: Pubkey::find_program_address(&[b"treasury"], &program.id()).0,
+            depositor: payer.pubkey(),
+            system_program: anchor_client::solana_sdk::system_program::ID,
+        })
+        .args(accounting_contract::instruction::DepositFunds {
+            amount: 1_000_000_000,
+        })
+        .send()
+        .expect("Failed to send transaction");
+
+    let treasury_account = program
+        .rpc()
+        .get_account(&Pubkey::find_program_address(&[b"treasury"], &program.id()).0)
+        .unwrap();
+    let treasury_struct: accounting_contract::TreasuryAccount =
+        bincode::deserialize(&treasury_account.data[8..]).unwrap();
+
+    // 1002240 is the initial balance of the treasury account
+    assert_eq!(treasury_account.lamports, 1000946560);
+    // assert_eq!(treasury_struct.balance, 1_000_000_000);
+
+    let payer_account = program.rpc().get_account(&payer.pubkey()).unwrap();
+    let initial_balance = payer_account.lamports;
+
+    let keypair = Keypair::new();
+    let nonce: u64 = 1;
+
+    let tx = program
+        .request()
+        .accounts(accounting_contract::accounts::CreatePayRequest {
+            pay_out_request: Pubkey::find_program_address(
+                &[
+                    keypair.pubkey().to_bytes().as_ref(),
+                    &nonce.to_le_bytes(),
+                    b"payout_request",
+                ],
+                &program.id(),
+            )
+            .0,
+            account_nonce: Pubkey::find_program_address(
+                &[keypair.pubkey().to_bytes().as_ref(), b"nonce"],
+                &program.id(),
+            )
+            .0,
+            signer: payer.pubkey(),
+            worker: Pubkey::find_program_address(&[b"worker"], &program.id()).0,
+            treasury: Pubkey::find_program_address(&[b"treasury"], &program.id()).0,
+            beneficiary: keypair.pubkey(),
+            system_program: anchor_client::solana_sdk::system_program::ID,
+        })
+        .args(accounting_contract::instruction::CreatePayRequest {
+            amount: 1_000_000,
+            nonce: 1_u64,
+        })
+        .send()
+        .expect("Failed to send transaction");
+
+    let payer_account = program.rpc().get_account(&keypair.pubkey()).unwrap();
+    let final_balance = payer_account.lamports;
+
+    assert_eq!(final_balance, 1_000_000);
 }
