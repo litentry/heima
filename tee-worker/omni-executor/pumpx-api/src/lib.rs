ec234562ddd478bdd779ca25489591c60790c3ab
Francisco J Silva <franjs.francisco@gmail.com>
2025-03-21 14:37:33 +0000

    feat(api): adding pumpx API client type definitions (WIP)

--- tee-worker/omni-executor/pumpx-api/src/lib.rs
+++ tee-worker/omni-executor/pumpx-api/src/lib.rs
@@ -1,3 +1,5 @@
+mod types;
+
 use reqwest::{
 	header::{HeaderMap, HeaderValue},
 };
---
+++ tee-worker/omni-executor/pumpx-api/src/types.rs
@@ -0,0 +1,37 @@
+use serde::{Deserialize, Serialize};
+use serde_repr::{Deserialize_repr, Serialize_repr};
+
+#[derive(Deserialize_repr, Serialize_repr)]
+#[allow(clippy::upper_case_acronyms)]
+#[repr(u8)]
+pub enum ChainType {
+	EVM = 1,
+	Solana = 2,
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Wallet {
+	pub user_wallet_address: String,
+	pub chain_type: ChainType,
+	pub wallet_index: u32,
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct NewUser {
+	pub invite_code: String,
+	pub wallet_list: Vec<Wallet>,
+	pub email: Option<String>,
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ApiResponse<T> {
+	code: u32,
+	message: String,
+	data: T,
+}
+
+#[derive(Deserialize)]
+pub struct EmptyData {}
