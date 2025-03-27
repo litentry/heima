27abb0b230123c047f8a12a83e6b05d4b1f39972
Francisco Silva <franjs.francisco@gmail.com>
2025-03-27 11:20:17 +0100

    Update JWT token issuance, Adding request_pumpx_jwt native call (#3308)

--- tee-worker/omni-executor/heima/authentication/src/auth_token.rs
+++ tee-worker/omni-executor/heima/authentication/src/auth_token.rs
@@ -1,6 +1,10 @@
 use executor_crypto::jwt;
 use executor_primitives::BlockNumber;
 use parity_scale_codec::{Decode, Encode};
+use rsa::{
+	pkcs1::{DecodeRsaPrivateKey, EncodeRsaPublicKey},
+	RsaPrivateKey,
+};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, PartialEq)]
@@ -11,9 +15,12 @@ pub enum Error {
 	InvalidSubject,
 	Base64DecodeError,
 	JsonError,
+	InternalError,
 }
 
 pub const AUTH_TOKEN_EXPIRATION: u32 = 50_400; // 1 week in blocks
+pub const AUTH_TOKEN_SESSION_TYPE: &str = "session_token";
+pub const AUTH_TOKEN_TRADE_TYPE: &str = "trade_token"; // Used by pumpx
 
 #[derive(Encode, Decode, Clone, Debug, PartialEq, Eq)]
 pub struct AuthOptions {
@@ -23,12 +30,13 @@ pub struct AuthOptions {
 #[derive(PartialEq, Debug, Serialize, Deserialize)]
 pub struct AuthTokenClaims {
 	sub: String,
+	typ: String,
 	pub exp: BlockNumber,
 }
 
 impl AuthTokenClaims {
-	pub fn new(sub: String, options: AuthOptions) -> Self {
-		Self { sub, exp: options.expires_at }
+	pub fn new(sub: String, typ: String, options: AuthOptions) -> Self {
+		Self { sub, typ, exp: options.expires_at }
 	}
 }
 
@@ -60,16 +68,28 @@ pub trait AuthTokenValidator {
 }
 
 impl AuthTokenValidator for String {
-	fn validate(&self, secret: &[u8], validation: Validation) -> Result<(), Error> {
-		jwt::decode::<AuthTokenClaims>(self, secret)
+	fn validate(&self, private_key: &[u8], validation: Validation) -> Result<(), Error> {
+		let rsa_private_key =
+			RsaPrivateKey::from_pkcs1_der(private_key).map_err(|_| Error::InternalError)?;
+		let public_key = rsa_private_key
+			.to_public_key()
+			.to_pkcs1_der()
+			.map_err(|_| Error::InternalError)?;
+		jwt::decode::<AuthTokenClaims>(self, public_key.as_bytes())
 			.map_err(|_| Error::InvalidToken)
 			.and_then(|claims| validation.validate(&claims))
 	}
 }
 
 impl AuthTokenValidator for &str {
-	fn validate(&self, secret: &[u8], validation: Validation) -> Result<(), Error> {
-		jwt::decode::<AuthTokenClaims>(self, secret)
+	fn validate(&self, private_key: &[u8], validation: Validation) -> Result<(), Error> {
+		let rsa_private_key =
+			RsaPrivateKey::from_pkcs1_der(private_key).map_err(|_| Error::InternalError)?;
+		let public_key = rsa_private_key
+			.to_public_key()
+			.to_pkcs1_der()
+			.map_err(|_| Error::InternalError)?;
+		jwt::decode::<AuthTokenClaims>(self, public_key.as_bytes())
 			.map_err(|_| Error::InvalidToken)
 			.and_then(|claims| validation.validate(&claims))
 	}
@@ -78,42 +98,67 @@ impl AuthTokenValidator for &str {
 #[cfg(test)]
 mod tests {
 	use super::*;
+	use rsa::{pkcs1::EncodeRsaPrivateKey, RsaPrivateKey};
 
 	#[test]
 	fn test_auth_token() {
-		let secret = b"secret";
-		let claims = AuthTokenClaims::new("test".to_string(), AuthOptions { expires_at: 100 });
-		let token = jwt::create(&claims, secret).unwrap();
+		let mut rng = rand::thread_rng();
+		let rsa_private_key =
+			RsaPrivateKey::new(&mut rng, 2048).expect("Failed to generate private key");
+		let private_key = rsa_private_key.to_pkcs1_der().unwrap();
+
+		let claims = AuthTokenClaims::new(
+			"test".to_string(),
+			AUTH_TOKEN_SESSION_TYPE.to_string(),
+			AuthOptions { expires_at: 100 },
+		);
+		let token = jwt::create(&claims, private_key.as_bytes()).unwrap();
 
 		let current_block = 50;
 		let validation = Validation::new("test".to_string(), current_block);
-		let result = token.validate(secret, validation);
+		let result = token.validate(private_key.as_bytes(), validation);
 
 		assert_eq!(result, Ok(()));
 	}
 
 	#[test]
 	fn test_auth_token_expired() {
-		let secret = b"secret";
-		let claims = AuthTokenClaims::new("test".to_string(), AuthOptions { expires_at: 100 });
-		let token = jwt::create(&claims, secret).unwrap();
+		let mut rng = rand::thread_rng();
+		let rsa_private_key =
+			RsaPrivateKey::new(&mut rng, 2048).expect("Failed to generate private key");
+		let private_key = rsa_private_key.to_pkcs1_der().unwrap();
+
+		let claims = AuthTokenClaims::new(
+			"test".to_string(),
+			AUTH_TOKEN_SESSION_TYPE.to_string(),
+			AuthOptions { expires_at: 100 },
+		);
+		let token = jwt::create(&claims, private_key.as_bytes()).unwrap();
 
 		let current_block = 150;
 		let validation = Validation::new("test".to_string(), current_block);
-		let result = token.validate(secret, validation);
+		let result = token.validate(private_key.as_bytes(), validation);
 
 		assert_eq!(result, Err(Error::ExpiredToken));
 	}
 
 	#[test]
 	fn test_auth_token_invalid_subject() {
-		let secret = b"secret";
-		let claims = AuthTokenClaims::new("test".to_string(), AuthOptions { expires_at: 100 });
-		let token = jwt::create(&claims, secret).unwrap();
+		let mut rng = rand::thread_rng();
+		let rsa_private_key =
+			RsaPrivateKey::new(&mut rng, 2048).expect("Failed to generate private key");
+		let private_key = rsa_private_key.to_pkcs1_der().unwrap();
+
+		let claims = AuthTokenClaims::new(
+			"test".to_string(),
+			AUTH_TOKEN_SESSION_TYPE.to_string(),
+			AuthOptions { expires_at: 100 },
+		);
+		let token = jwt::create(&claims, private_key.as_bytes()).unwrap();
 
 		let current_block = 50;
 		let validation = Validation::new("invalid-sub".to_string(), current_block);
-		let result = token.validate(secret, validation);
+		let result = token.validate(private_key.as_bytes(), validation);
 
 		assert_eq!(result, Err(Error::InvalidSubject));
 	}
---
+++ tee-worker/omni-executor/rpc-server/src/auth_token_key_store.rs
@@ -0,0 +1,60 @@
+use executor_core::key_store::KeyStore;
+use executor_crypto::rsa::RsaPrivateKey;
+use rsa::pkcs1::{DecodeRsaPrivateKey, EncodeRsaPrivateKey, EncodeRsaPublicKey};
+
+pub struct AuthTokenKeyStore {
+	path: String,
+}
+
+impl AuthTokenKeyStore {
+	pub fn new(path: String) -> Self {
+		let store = Self { path: path.clone() };
+		let std_path = std::path::Path::new(&path);
+		if !std_path.exists() {
+			let key: Vec<u8> = Self::generate_key().unwrap();
+			// Create store file if not exists.
+			if let Some(parent) = std_path.parent() {
+				if !parent.exists() {
+					std::fs::create_dir_all(parent).unwrap();
+				}
+			}
+			let _file = std::fs::File::create(&path).unwrap();
+			store.write(&key).unwrap();
+		}
+
+		store
+	}
+}
+
+impl KeyStore<Vec<u8>> for AuthTokenKeyStore {
+	fn generate_key() -> Result<Vec<u8>, ()> {
+		let mut rng = rand::thread_rng();
+		let rsa_private_key = RsaPrivateKey::new(&mut rng, 2048).map_err(|e| {
+			log::error!("Failed to generate RSA key: {:?}", e);
+		})?;
+		let private_key = rsa_private_key.to_pkcs1_der().map_err(|e| {
+			log::error!("Failed to encode RSA key: {:?}", e);
+		})?;
+		Ok(private_key.as_bytes().to_vec())
+	}
+
+	fn serialize(k: &Vec<u8>) -> Result<Vec<u8>, ()> {
+		Ok(k.to_vec())
+	}
+
+	fn deserialize(sealed: Vec<u8>) -> Result<Vec<u8>, ()> {
+		let private_key = RsaPrivateKey::from_pkcs1_der(&sealed).map_err(|e| {
+			log::error!("Failed to decode RSA key: {:?}", e);
+		})?;
+		let public_key = private_key.to_public_key().to_pkcs1_der().map_err(|e| {
+			log::error!("Failed to encode RSA key: {:?}", e);
+		})?;
+		log::info!("Auth token (JWT) RSA public_key: {:?}", public_key);
+
+		Ok(sealed)
+	}
+
+	fn path(&self) -> String {
+		self.path.clone()
+	}
+}
