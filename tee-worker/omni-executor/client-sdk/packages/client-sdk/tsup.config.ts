95ae6ebcaf6294a2bb20924bf6a31d31ad1748f0
0xverin <daqingchong0809@gmail.com>
2025-03-27 17:41:51 +0800

    fix alias path

--- tee-worker/omni-executor/client-sdk/packages/client-sdk/tsup.config.ts
+++ tee-worker/omni-executor/client-sdk/packages/client-sdk/tsup.config.ts
@@ -5,26 +5,14 @@ import path from 'node:path';
 export default defineConfig([
   {
     entry: ['src'],
-    outDir: 'dist/packages/client-sdk',
+    outDir: '../../dist/packages/client-sdk',
     format: ['esm', 'cjs'],
     splitting: false,
     sourcemap: true,
     clean: true,
     treeshake: true,
-    esbuildOptions(options) {
-      options.alias = {
-        '@lib': path.resolve(__dirname, 'src/lib'),
-        '@requests': path.resolve(__dirname, 'src/lib/requests'),
-        '@type-creators': path.resolve(__dirname, 'src/lib/type-creators'),
-        '@utils': path.resolve(__dirname, 'src/lib/utils'),
-        '@test-utils': path.resolve(__dirname, 'src/lib/test-utils'),
-      };
-    },
-    onSuccess: async () => {
-      console.log('ESM/CJS build successful!');
-      execSync('pnpm tsc -p ./tsconfig.lib.json --emitDeclarationOnly', {
-        stdio: 'inherit',
-      });
+    dts: {
+      resolve: true,
     },
   },
 ]);
