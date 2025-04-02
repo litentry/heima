5e4dd1658d671885ca23c9e85343c95c60ef1a83
wli-pro <wli-pro>
2025-04-02 16:04:05 +1100

    fix NativeTaskError definition

--- tee-worker/client-api/parachain-api/prepare-build/interfaces/omniExecutor/definitions.ts
+++ tee-worker/client-api/parachain-api/prepare-build/interfaces/omniExecutor/definitions.ts
@@ -118,6 +118,8 @@ export default {
                 InternalError: "Null",
                 InvalidMemberIdentity: "Null",
                 ValidationDataVerificationFailed: "Null",
+                UnsupportedIdentityType: "Null",
+                PumpxApiError: "Null",
             },
         },
         HeimaMultiSignature: {
