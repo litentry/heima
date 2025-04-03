7652ac2513e7ec7762684062096deb19d298bf2a
Francisco J Silva <franjs.francisco@gmail.com>
2025-04-03 14:50:24 +0000

    feat(jwt): implement structured JWT response handling
    Refactors the pumpx_requestJwt RPC method to return structured JWT
    response data instead of hex-encoded bytes. Adds a RequestJwtResponse
    type to properly serialize access token, ID token and user connection
    data. Improves error handling with specific error codes and better
    logging for different failure scenarios.

--- tee-worker/omni-executor/rpc-server/src/methods/pumpx/request_jwt.rs
+++ tee-worker/omni-executor/rpc-server/src/methods/pumpx/request_jwt.rs
@@ -1,11 +1,14 @@
 use crate::{
-	error_code::*, hex_encode, oneshot, server::RpcContext, verify_auth::verify_auth, Deserialize,
+	error_code::*, oneshot, server::RpcContext, verify_auth::verify_auth, Decode, Deserialize,
 	ErrorCode,
 };
 use executor_core::native_task::*;
 use executor_primitives::OmniAuth;
 use heima_primitives::{Identity, Web2IdentityType};
-use jsonrpsee::{types::ErrorObject, RpcModule};
+use jsonrpsee::RpcModule;
+use native_task_handler::{NativeTaskError, NativeTaskOk, NativeTaskResponse};
+use pumpx::types::UserConnectResponse;
+use serde::Serialize;
 
 #[derive(Debug, Deserialize)]
 pub struct RequestJwtParams {
@@ -14,6 +17,13 @@ pub struct RequestJwtParams {
 	pub google_code: MaybeGoogleCode,
 	pub language: Option<String>,
 	pub email_code: String,
+}
+
+#[derive(Serialize, Clone)]
+pub struct RequestJwtResponse {
+	pub access_token: String,
+	pub id_token: String,
+	pub user_connect_response: UserConnectResponse,
 }
 
 impl From<RequestJwtParams> for NativeTaskWrapper<NativeTask> {
@@ -34,25 +44,51 @@ impl From<RequestJwtParams> for NativeTaskWrapper<NativeTask> {
 pub fn register_request_jwt(module: &mut RpcModule<RpcContext>) {
 	module
 		.register_async_method("pumpx_requestJwt", |params, ctx, _| async move {
-			let params = params.parse::<RequestJwtParams>()?;
+			let params = params.parse::<RequestJwtParams>().map_err(|_| ErrorCode::ParseError)?;
 			let wrapper: NativeTaskWrapper<NativeTask> = params.into();
 
 			if wrapper.task.require_auth() && verify_auth(ctx.clone(), &wrapper).await.is_err() {
-				return Err(ErrorCode::ServerError(AUTH_VERIFICATION_FAILED_CODE).into());
+				return Err(ErrorCode::ServerError(AUTH_VERIFICATION_FAILED_CODE));
 			}
 
 			let (response_sender, response_receiver) = oneshot::channel();
 
 			if ctx.native_task_sender.send((wrapper, response_sender)).await.is_err() {
 				log::error!("Failed to send request to native call executor");
-				return Err(ErrorCode::InternalError.into());
+				return Err(ErrorCode::InternalError);
 			}
 			match response_receiver.await {
-				// TODO: we should use json field for better readability, either a defined structure, or serde_json::Value
-				Ok(response) => Ok::<String, ErrorObject>(hex_encode(response.as_slice())),
+				Ok(response) => {
+					let native_task_response: NativeTaskResponse =
+						Decode::decode(&mut response.as_slice())
+							.map_err(|_| ErrorCode::InternalError)?;
+					match native_task_response {
+						Ok(NativeTaskOk::PumpxJwt {
+							access_token,
+							id_token,
+							user_connect_response,
+						}) => {
+							Ok(RequestJwtResponse { access_token, id_token, user_connect_response })
+						},
+						Err(NativeTaskError::InternalError) => {
+							log::error!("Internal error in native task");
+							Err(ErrorCode::InternalError)
+						},
+						Err(native_task_error) => {
+							log::error!("Native task error: {:?}", native_task_error);
+							Err(ErrorCode::ServerError(get_native_task_error_code(
+								&native_task_error,
+							)))
+						},
+						_ => {
+							log::error!("Unexpected response type");
+							Err(ErrorCode::InternalError)
+						},
+					}
+				},
 				Err(e) => {
 					log::error!("Failed to receive response from native call handler: {:?}", e);
-					Err(ErrorCode::InternalError.into())
+					Err(ErrorCode::InternalError)
 				},
 			}
 		})
