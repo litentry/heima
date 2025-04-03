5372db8ad7d61ff8cd52d8129c99f3cda9782387
Francisco J Silva <franjs.francisco@gmail.com>
2025-04-03 09:40:37 +0000

    feat(crypto): use expiration dates in JWT tokens

--- tee-worker/omni-executor/executor-crypto/src/jwt.rs
+++ tee-worker/omni-executor/executor-crypto/src/jwt.rs
@@ -1,3 +1,4 @@
+pub use jsonwebtoken::errors::{Error, ErrorKind};
 use jsonwebtoken::{
 	decode as decode_jwt, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation,
 };
@@ -12,19 +13,16 @@ pub fn create<T: Serialize>(claims: &T, private_key: &[u8]) -> Result<String, St
 	})
 }
 
-pub fn decode<T: DeserializeOwned>(token: &str, public_key: &[u8]) -> Result<T, String> {
-	let mut validation = Validation::new(Algorithm::RS256);
-	validation.set_required_spec_claims(&["sub"]);
-	validation.validate_exp = false;
+pub fn decode<T: DeserializeOwned>(token: &str, public_key: &[u8]) -> Result<T, Error> {
+	let validation = Validation::new(Algorithm::RS256);
 	let decoding_key = DecodingKey::from_rsa_der(public_key);
-	decode_jwt::<T>(token, &decoding_key, &validation)
-		.map(|data| data.claims)
-		.map_err(|e| e.to_string())
+	decode_jwt::<T>(token, &decoding_key, &validation).map(|data| data.claims)
 }
 
 #[cfg(test)]
 mod tests {
 	use super::*;
+	use chrono::{Days, Utc};
 	use rsa::{
 		pkcs1::{EncodeRsaPrivateKey, EncodeRsaPublicKey},
 		RsaPrivateKey,
@@ -33,6 +31,7 @@ mod tests {
 	#[derive(serde::Serialize, serde::Deserialize, Debug, PartialEq)]
 	struct JwtClaims {
 		pub sub: String,
+		pub exp: i64,
 	}
 
 	#[test]
@@ -43,7 +42,11 @@ mod tests {
 		let private_key = rsa_private_key.to_pkcs1_der().unwrap();
 		let public_key = rsa_private_key.to_public_key().to_pkcs1_der().unwrap();
 
-		let claims = JwtClaims { sub: "test".to_string() };
+		let exp = Utc::now()
+			.checked_add_days(Days::new(1))
+			.expect("Failed to calculate expiration")
+			.timestamp();
+		let claims = JwtClaims { sub: "test".to_string(), exp };
 
 		let token = create(&claims, private_key.as_bytes()).unwrap();
 		let decoded = decode::<JwtClaims>(&token, public_key.as_bytes()).unwrap();
