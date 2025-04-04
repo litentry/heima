cd256ce77fde085e3510f6a4693fd73277767a30
Kasper Ziemianek <kasper.ziemianek@gmail.com>
2025-04-04 11:51:53 +0200

    intent id store

---
+++ tee-worker/omni-executor/intent/core/src/lib.rs
@@ -0,0 +1,27 @@
+use async_trait::async_trait;
+use executor_primitives::AccountId;
+use std::collections::HashMap;
+use tokio::sync::RwLock;
+
+pub type IntentId = u128;
+
+#[async_trait]
+pub trait IntentIdStore {
+	async fn get(&self, account_id: &AccountId) -> Result<Option<IntentId>, ()>;
+	async fn update(&self, account_id: AccountId, id: IntentId) -> Result<(), ()>;
+}
+
+pub struct InMemoryIntentIdStore {
+	inner: RwLock<HashMap<AccountId, IntentId>>,
+}
+#[async_trait]
+impl IntentIdStore for InMemoryIntentIdStore {
+	async fn get(&self, account_id: &AccountId) -> Result<Option<IntentId>, ()> {
+		Ok(self.inner.read().await.get(account_id).copied())
+	}
+
+	async fn update(&self, account_id: AccountId, id: IntentId) -> Result<(), ()> {
+		self.inner.write().await.insert(account_id, id);
+		Ok(())
+	}
+}
