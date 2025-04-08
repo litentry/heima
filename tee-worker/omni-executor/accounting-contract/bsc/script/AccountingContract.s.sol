2cbe81ae36fe09c3f17b6cc09cb69d8dad380f69
Faisal Ahmed <faisalahmedfarooq46@gmail.com>
2025-04-08 12:39:56 +0530

    refactor: remove comments

--- tee-worker/omni-executor/accounting-contract/bsc/script/AccountingContract.s.sol
+++ tee-worker/omni-executor/accounting-contract/bsc/script/AccountingContract.s.sol
@@ -38,7 +38,6 @@ contract DeployContract is Script {
     }
 }
 
-// export PRIVATE_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 && export CONTRACT=0x5FbDB2315678afecb367f032d93F642f64180aa3 && forge script script/AccountingContract.s.sol:DeployContract --rpc-url http://localhost:8545 --broadcast
 contract DepositFunds is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
