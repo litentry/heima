cc0abd1cf7e2adcab542a8ac34c318bf52bd25b1
0xverin <daqingchong0809@gmail.com>
2025-04-08 20:07:11 +0800

    update add & export wallet example code

---
+++ tee-worker/omni-executor/client-sdk/packages/client-sdk/src/lib/utils/rpc-method.ts
@@ -0,0 +1,6 @@
+export const enum PumpxRpcMethod {
+  RequestEmailVerificationCode = 'pumpx_requestEmailVerificationCode',
+  RequestJwt = 'pumpx_requestJwt',
+  ExportWallet = 'pumpx_exportWallet',
+  AddWallet = 'pumpx_addWallet',
+}
