72920df523e2f9b64819ec77478ef8a109f9a00f
Francisco J Silva <franjs.francisco@gmail.com>
2025-04-08 19:20:09 +0000

    initial setup rpc method to submit swaps

--- tee-worker/omni-executor/rpc-server/src/methods/pumpx/mod.rs
+++ tee-worker/omni-executor/rpc-server/src/methods/pumpx/mod.rs
@@ -7,7 +7,11 @@ use request_jwt::*;
 mod export_wallet;
 use export_wallet::*;
 
+mod submit_swap_order;
+use submit_swap_order::*;
+
 pub fn register_pumpx(module: &mut RpcModule<RpcContext>) {
 	register_request_jwt(module);
 	register_export_wallet(module);
+	register_submit_swap_order(module);
 }
