7907054135efff6b23690c7d3df3a5e62fc86e43
Francisco J Silva <franjs.francisco@gmail.com>
2025-04-10 12:57:34 +0000

    commenting out rest of locking mechanism

--- tee-worker/omni-executor/intent/executors/cross-chain/src/lib.rs
+++ tee-worker/omni-executor/intent/executors/cross-chain/src/lib.rs
@@ -24,12 +24,12 @@ use executor_primitives::SingleChainSwapProvider;
 use executor_storage::StorageDB;
 use executor_storage::{PumpxJwtStorage, Storage};
 use heima_authentication::auth_token::AUTH_TOKEN_ACCESS_TYPE;
-use intent_asset_lock::AmountType;
-use intent_token_query::query_ethereum;
-use intent_token_query::query_solana;
-use intent_token_query::EthereumAddress;
-use intent_token_query::SolanaPubkey;
-use log::error;
+// use intent_asset_lock::AmountType;
+// use intent_token_query::query_ethereum;
+// use intent_token_query::query_solana;
+// use intent_token_query::EthereumAddress;
+// use intent_token_query::SolanaPubkey;
+// use log::error;
 use parity_scale_codec::Encode;
 use pumpx::signer_client::SignerClient;
 use pumpx::types::ChainId;
@@ -56,8 +56,8 @@ use parentchain_rpc_client::SubxtClientFactory;
 use parentchain_rpc_client::ToSubxtType;
 use parentchain_signer::TxSigner;
 
-use intent_asset_lock::always_unlocked::AlwaysUnlockedAssetsLock;
-use intent_asset_lock::AccountAssetLocks;
+// use intent_asset_lock::always_unlocked::AlwaysUnlockedAssetsLock;
+// use intent_asset_lock::AccountAssetLocks;
 
 #[derive(PartialEq, Hash, Eq)]
 pub enum Chain {
@@ -83,8 +83,8 @@ pub struct CrossChainIntentExecutor<
 > {
 	parentchain_rpc_client_factory: Arc<RpcClientFactory>,
 	transaction_signer: Arc<ParentchainTxSigner>,
-	account_asset_lock: AccountAssetLocks<AlwaysUnlockedAssetsLock>,
-	rpc_endpoint_registry: RpcEndpointRegistry,
+	// account_asset_lock: AccountAssetLocks<AlwaysUnlockedAssetsLock>,
+	// rpc_endpoint_registry: RpcEndpointRegistry,
 	pumpx_signer_client: Arc<SignerClient>,
 	pumpx_api: Arc<PumpxApi>,
 	storage_db: Arc<StorageDB>,
@@ -100,18 +100,18 @@ impl<
 	pub fn new(
 		parentchain_rpc_client_factory: Arc<RpcClientFactory>,
 		transaction_signer: Arc<ParentchainTxSigner>,
-		rpc_endpoint_registry: RpcEndpointRegistry,
+		_rpc_endpoint_registry: RpcEndpointRegistry,
 		pumpx_signer_client: Arc<SignerClient>,
 		pumpx_api: Arc<PumpxApi>,
 		storage_db: Arc<StorageDB>,
 	) -> Result<Self, ()> {
 		// there is no need for account/assets locks if we guarantee the dest-chain payout happens after the source chain finalisation
-		let account_asset_lock = AccountAssetLocks::<AlwaysUnlockedAssetsLock>::empty();
+		// let account_asset_lock = AccountAssetLocks::<AlwaysUnlockedAssetsLock>::empty();
 		Ok(Self {
 			parentchain_rpc_client_factory,
 			transaction_signer,
-			account_asset_lock,
-			rpc_endpoint_registry,
+			// account_asset_lock,
+			// rpc_endpoint_registry,
 			pumpx_signer_client,
 			pumpx_api,
 			storage_db,
@@ -140,48 +140,43 @@ impl<
 					log::error!("Failed to create rpc client");
 					return Err(());
 				};
-				let available_amount = match &swap_order.from_asset {
-					ChainAsset::Ethereum(chain_id, token) => {
-						let rpc_url =
-							self.rpc_endpoint_registry.get(&Chain::Ethereum(*chain_id)).ok_or(())?;
-						let address = self
-							.pumpx_signer_client
-							.request_wallet(
-								pumpx::signer_client::ChainType::Evm,
-								0,
-								*account_id.as_ref(),
-							)
-							.await
-							.map_err(|e| {
-								error!("Could not get wallet from pumpx-signer: {:?}", e)
-							})?;
-						query_ethereum(rpc_url, EthereumAddress::from_slice(&address), token)
-							.await?
-					},
-					ChainAsset::Solana(token) => {
-						let rpc_url = self.rpc_endpoint_registry.get(&Chain::Solana).ok_or(())?;
-						let address = self
-							.pumpx_signer_client
-							.request_wallet(
-								pumpx::signer_client::ChainType::Solana,
-								0,
-								*account_id.as_ref(),
-							)
-							.await
-							.map_err(|e| {
-								error!("Could not get wallet from pumpx-signer: {:?}", e)
-							})?;
-						let pubkey = SolanaPubkey::try_from(address).map_err(|e| {
-							error!("Could not create solana pubkey from wallet address: {:?}", e)
-						})?;
-						query_solana(rpc_url, &pubkey, token).await.map(|v| AmountType::from(v))?
-					},
-				};
-				let from_amount_string = std::str::from_utf8(&swap_order.from_amount)
-					.map_err(|_| {
-						log::error!("Failed to parse from_amount_string");
-					})
-					.map(|v| v.to_string())?;
+				// let available_amount = match &swap_order.from_asset {
+				// 	ChainAsset::Ethereum(chain_id, token) => {
+				// 		let rpc_url =
+				// 			self.rpc_endpoint_registry.get(&Chain::Ethereum(*chain_id)).ok_or(())?;
+				// 		let address = self
+				// 			.pumpx_signer_client
+				// 			.request_wallet(
+				// 				pumpx::signer_client::ChainType::Evm,
+				// 				0,
+				// 				*account_id.as_ref(),
+				// 			)
+				// 			.await
+				// 			.map_err(|e| {
+				// 				error!("Could not get wallet from pumpx-signer: {:?}", e)
+				// 			})?;
+				// 		query_ethereum(rpc_url, EthereumAddress::from_slice(&address), token)
+				// 			.await?
+				// 	},
+				// 	ChainAsset::Solana(token) => {
+				// 		let rpc_url = self.rpc_endpoint_registry.get(&Chain::Solana).ok_or(())?;
+				// 		let address = self
+				// 			.pumpx_signer_client
+				// 			.request_wallet(
+				// 				pumpx::signer_client::ChainType::Solana,
+				// 				0,
+				// 				*account_id.as_ref(),
+				// 			)
+				// 			.await
+				// 			.map_err(|e| {
+				// 				error!("Could not get wallet from pumpx-signer: {:?}", e)
+				// 			})?;
+				// 		let pubkey = SolanaPubkey::try_from(address).map_err(|e| {
+				// 			error!("Could not create solana pubkey from wallet address: {:?}", e)
+				// 		})?;
+				// 		query_solana(rpc_url, &pubkey, token).await.map(|v| AmountType::from(v))?
+				// 	},
+				// };
 
 				// self.account_asset_lock.check_and_insert(
 				// 	account_id.clone(),
@@ -189,6 +184,13 @@ impl<
 				// 	AmountType::from(from_amount),
 				// 	available_amount,
 				// )?;
+				//
+
+				let from_amount_string = std::str::from_utf8(&swap_order.from_amount)
+					.map_err(|_| {
+						log::error!("Failed to parse from_amount_string");
+					})
+					.map(|v| v.to_string())?;
 
 				let intent_accepted_event_emit_call = parentchain_api_interface::tx()
 					.omni_account()
