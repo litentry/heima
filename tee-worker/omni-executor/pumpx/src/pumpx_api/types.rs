7b834e66f0db351a6d3cb0d23532c40e93f394fa
Kailai-Wang <Kailai.Wang@hotmail.com>
2025-04-13 14:31:40 +0000

    add new api

--- tee-worker/omni-executor/pumpx/src/pumpx_api/types.rs
+++ tee-worker/omni-executor/pumpx/src/pumpx_api/types.rs
@@ -22,6 +22,7 @@ pub type SendTransferTxResponse = ApiResponse<SendTransferTxResponseData>;
 // new API
 pub type CreateMarketOrderTxResponse = ApiResponse<CreateMarketOrderTxResponseData>;
 pub type CreateTransferTxResponse = ApiResponse<CreateTransferTxResponseData>;
+pub type GetGasInfoResponse = ApiResponse<GetGasInfoResponseData>;
 
 #[derive(Deserialize_repr, Serialize_repr, Debug)]
 #[allow(clippy::upper_case_acronyms)]
@@ -303,4 +304,32 @@ pub struct CrossOrderInfo {
 pub struct CrossFailBody {
 	pub request_id: u32,
 	pub fail_reason: String,
+}
+
+// /v1/trade/get_gas_info
+#[derive(Serialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct GetGasInfoBody {
+	pub chain_id: u32,
+}
+
+#[derive(Serialize, Deserialize, Encode, Decode, Debug, PartialEq, Eq, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct GetGasInfoResponseData {
+	pub gas_info: GasInfo,
+}
+
+#[derive(Serialize, Deserialize, Encode, Decode, Debug, PartialEq, Eq, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct GasInfo {
+	chain_id: String,
+	normal: String,
+	fast: String,
+	super_fast: String,
+	normal_usd: String,
+	fast_usd: String,
+	super_fast_usd: String,
+	normal_price: String,
+	fast_price: String,
+	super_fast_price: String,
 }
