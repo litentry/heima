6913130ec6389bdc778b677b7d14cc4f19111659
Francisco J Silva <franjs.francisco@gmail.com>
2025-04-14 21:57:07 +0000

    feat(binance-api): add deposit history endpoint

--- tee-worker/omni-executor/binance-api/src/wallet_api/mod.rs
+++ tee-worker/omni-executor/binance-api/src/wallet_api/mod.rs
@@ -1,6 +1,6 @@
 mod types;
 
-use types::{CoinInfo, DepositAddress};
+use types::{CoinInfo, Deposit, DepositAddress};
 
 use crate::{error::Error, BinanceApi, Method};
 use std::collections::HashMap;
@@ -34,5 +34,18 @@ impl<'a> WalletApi<'a> {
 			.make_signed_request(&endpoint, Method::GET, Some(params), None)
 			.await?;
 		Ok(response.address)
+	}
+
+	/// Fetch deposit history
+	pub async fn get_deposit_history(&self, coin: Option<String>) -> Result<Vec<Deposit>, Error> {
+		let endpoint = format!("{}/capital/deposit/hisrec", WALLET_API);
+		let mut params = HashMap::new();
+		params.insert("includeSource".to_string(), "true".to_string());
+		if let Some(coin) = coin {
+			params.insert("coin".to_string(), coin);
+		}
+		self.base_api
+			.make_signed_request(&endpoint, Method::GET, Some(params), None)
+			.await
 	}
 }
