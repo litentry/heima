466e7718ac97d0ec84add117c478574fbddc0cc2
Kasper Ziemianek <kasper.ziemianek@gmail.com>
2025-04-15 10:39:35 +0200

    read accounting contract address from cli

--- tee-worker/omni-executor/executor-worker/src/main.rs
+++ tee-worker/omni-executor/executor-worker/src/main.rs
@@ -209,8 +209,7 @@ async fn main() -> Result<(), ()> {
 			);
 			let accounting_contract_client = AccountingContractClient::new(
 				ethereum_rpc_provider,
-				//todo: from CLI
-				"0xb0830ef478a215ed393c20a0c97aa69869a0beea".parse().unwrap(),
+				args.accounting_contract_address.parse().unwrap(),
 			);
 
 			let cross_chain_intent_executor = CrossChainIntentExecutor::new(
