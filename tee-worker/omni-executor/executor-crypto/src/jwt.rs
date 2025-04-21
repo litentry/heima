9807451d853a611857a16d737a5c1f5225bf91bc
Francisco J Silva <franjs.francisco@gmail.com>
2025-04-21 14:33:06 +0000

    feat(jwt): require "sub" and "typ" claims in JWT validation

--- tee-worker/omni-executor/executor-crypto/src/jwt.rs
+++ tee-worker/omni-executor/executor-crypto/src/jwt.rs
@@ -22,6 +22,7 @@ pub fn decode<T: DeserializeOwned>(
 	if skip_exp_check {
 		validation.validate_exp = false;
 	}
+	validation.set_required_spec_claims(&["sub", "typ"]);
 	let decoding_key = DecodingKey::from_rsa_der(public_key);
 	decode_jwt::<T>(token, &decoding_key, &validation).map(|data| data.claims)
 }
