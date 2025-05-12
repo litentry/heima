562afd0eeecb9404db814a640b15b3ed1345b0e8
0xverin <daqingchong0809@gmail.com>
2025-05-12 16:53:30 +0800

    refactor: standardize metadata file paths and update package dependencies

--- tee-worker/identity/cli/lit_ts_api_package_build.sh
+++ tee-worker/identity/cli/lit_ts_api_package_build.sh
@@ -44,12 +44,12 @@ echo "Using trusted-worker uri $WORKER1URL:$WORKER1PORT"
 echo "Using node http uri $NODEHTTPURL:$NPORT"
 echo "waiting 20 secs worker to run successfully"
 sleep 20
-cd /client-api/parachain-api
-curl -s -H "Content-Type: application/json" -d '{"id": "1", "jsonrpc": "2.0", "method": "state_getMetadata", "params": []}' $NODEHTTPURL:$NPORT > prepare-build/litentry-parachain-metadata.json
+cd /client-api
+curl -s -H "Content-Type: application/json" -d '{"id": "1", "jsonrpc": "2.0", "method": "state_getMetadata", "params": []}' $NODEHTTPURL:$NPORT > metadata-parachain.json
 echo "update parachain metadata"
 
 cd  /client-api/sidechain-api
-${CLIENT} print-sgx-metadata-raw > prepare-build/litentry-sidechain-metadata.json
+${CLIENT} print-sgx-metadata-raw > metadata-sidechain.json
 echo "update sidechain metadata"
 
 cd /client-api
