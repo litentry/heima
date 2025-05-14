743799dee3fa91186661b6774992a0534e9e909b
0xverin <daqingchong0809@gmail.com>
2025-05-14 16:10:49 +0800

    feat: extend CLI options and update metadata fetching in integration test script

--- tee-worker/omni-executor/cli/ts_integration_test.sh
+++ tee-worker/omni-executor/cli/ts_integration_test.sh
@@ -5,47 +5,69 @@
 # Enable strict error handling: exit on error (-e), error on undefined vars (-u), exit on pipe failures (-o pipefail)
 set -euo pipefail
 
-# Parse command line options:
-# -u: Node URL to connect to
-while getopts ":u" opt; do
+while getopts ":p:A:u:W:V:C:" opt; do
     case $opt in
-        u)
-            NODE_URL=$OPTARG
+        p)
+            NPORT=$OPTARG
             ;;
-        ?)
-            echo "Invalid option: -${OPTARG}."
-            exit 1
+        A)
+            WORKER1PORT=$OPTARG
+            ;;
+        u)
+            NODEURL=$OPTARG
+            ;;
+        W)
+            NODEHTTPURL=$OPTARG
+            ;;
+        V)
+            WORKER1URL=$OPTARG
+            ;;
+        C)
+            CLIENT_BIN=$OPTARG
             ;;
     esac
 done
 
-NODE_URL=${NODE_URL:-"http://heima-node:9944"}
-echo "Using node url $NODE_URL"
+# Using default port if none given as arguments.
+NPORT=${NPORT:-9944}
+NODEURL=${NODEURL:-"ws://heima-node"}
+NODEHTTPURL=${NODEHTTPURL:-"http://heima-node"}
+WORKER1PORT=${WORKER1PORT:-2011}
+WORKER1URL=${WORKER1URL:-"ws://litentry-worker-1"}
+
+CLIENT_BIN=${CLIENT_BIN:-"/usr/local/bin/litentry-cli"}
+
+CLIENT="${CLIENT_BIN} -p ${NPORT} -P ${WORKER1PORT} -u ${NODEURL} -U ${WORKER1URL}"
 
 function usage() {
     echo ""
-    echo "This is a script for omni-executor integration ts tests. Pass test name as first argument"
+    echo "This is a script for tee-worker integration ts tests. Pass test name as first argument"
     echo ""
+
 }
 
-# Exit with usage info if exactly one argument is not provided
 [ $# -ne 1 ] && (usage; exit 1)
-
 TEST=$1
 
-echo "Running integration tests for $TEST"
+echo "Using client binary $CLIENT_BIN"
+echo "Using node uri $NODEURL:$NPORT"
+echo "Using trusted-worker uri $WORKER1URL:$WORKER1PORT"
+echo "Using node http uri $NODEHTTPURL:$NPORT"
+echo ""
 
 cd /client-api
-curl -s -H "Content-Type: application/json" -d '{"id": "1", "jsonrpc": "2.0", "method": "state_getMetadata", "params": []}' $NODE_URL > metadata-parachain.json
-echo "Parachain metadata fetched"
+curl -s -H "Content-Type: application/json" -d '{"id": "1", "jsonrpc": "2.0", "method": "state_getMetadata", "params": []}' $NODEHTTPURL:$NPORT > metadata-parachain.json
+echo "update parachain metadata"
 
-# echo "Installing pnpm"
-# npm install --global corepack@latest
-# corepack enable pnpm
 
-echo "Installing dependencies and building client-api"
+# Due to the sidechain being built into the client-api, we need to get all the metadata before the client-api can be built normally.
+cd  /client-api
+${CLIENT} print-sgx-metadata-raw > metadata-sidechain.json
+echo "update sidechain metadata"
+
+
 cd /client-api
-pnpm install --force
+pnpm install
 pnpm run build
 
 echo "Installing dependencies and building ts-tests"
