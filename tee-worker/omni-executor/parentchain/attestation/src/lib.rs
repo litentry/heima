a8741c2f5eb56e9b303253015ee28b612412e491
Francisco J Silva <franjs.francisco@gmail.com>
2025-05-16 12:12:49 +0000

    fix clippy issue

--- tee-worker/omni-executor/parentchain/attestation/src/lib.rs
+++ tee-worker/omni-executor/parentchain/attestation/src/lib.rs
@@ -1,5 +1,5 @@
 use executor_primitives::MrEnclave;
-use log::{debug, info};
+use log::info;
 use parentchain_api_interface::{
 	runtime_types::core_primitives::teebag::types::DcapProvider,
 	teebag::calls::types::register_enclave::{AttestationType, WorkerMode, WorkerType},
@@ -45,7 +45,7 @@ pub async fn generate_attestation_data(signer: Keypair) -> Result<(Vec<u8>, MrEn
 
 		let dcap_quote: DcapQuote =
 			DcapQuote::decode(&mut quote.as_slice()).expect("Failed to decode quote");
-		debug!("Attestation dcap_quote {:?}", dcap_quote);
+		log::debug!("Attestation dcap_quote {:?}", dcap_quote);
 
 		mrenclave = dcap_quote.body.mr_enclave;
 	}
