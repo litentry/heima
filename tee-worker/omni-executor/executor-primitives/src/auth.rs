22c6a1638b393e04430fe878ad542f5b51583bf9
Francisco J Silva <franjs.francisco@gmail.com>
2025-06-04 13:46:34 +0000

    feat: add omni_account and client_id to Web3 authentication message format

--- tee-worker/omni-executor/executor-primitives/src/auth.rs
+++ tee-worker/omni-executor/executor-primitives/src/auth.rs
@@ -86,7 +86,7 @@ impl TryFrom<IdentitySerde> for Identity {
 
 #[derive(Encode, Decode, Clone, Debug, PartialEq, Eq)]
 pub enum OmniAuth {
-	Web3(Identity, HeimaMultiSignature), // (Signer, Signature)
+	Web3(String, Identity, HeimaMultiSignature), // (client_id, Signer, Signature)
 	Email(Email, VerificationCode),
 	AuthToken(JwtToken),
 	OAuth2(Identity, OAuth2Data), // (Sender, OAuth2Data)
