0944e9f42353b975770bba04940611b91473afe6
Francisco Silva <franjs.francisco@gmail.com>
2025-06-09 01:21:53 +0200

    Enable jwt authentication on protected methods (#3503)
    * feat: add JWT authentication to omni_addWallet method
    
    - Add omni_addWallet to PROTECTED_METHODS array
    - Remove auth_token parameter from AddWalletParams struct
    - Replace manual auth verification with check_auth helper
    - Use RpcExtensions from middleware for authentication
    - Add User struct to represent authenticated user context
    - Simplify NativeTaskWrapper creation by removing auth param
    
    The authentication is now handled consistently through the RPC
    middleware layer, extracting user info from JWT tokens rather
    than requiring explicit auth tokens in method parameters.
    
    * updating test methods
    
    * feat: add JWT auth to omni_notifyLimitOrderResult
    
    * feat: add JWT auth to omni_signLimitOrder method
    
    * refactor: clean up imports in omni_signLimitOrder
    
    * feat: add JWT auth to omni_submitSwapOrder method
    
    * make debug call simpler
    
    ---------
    
    Co-authored-by: Kai <7630809+Kailai-Wang@users.noreply.github.com>

--- tee-worker/omni-executor/rpc-server/src/middlewares/rpc_middleware.rs
+++ tee-worker/omni-executor/rpc-server/src/middlewares/rpc_middleware.rs
@@ -2,7 +2,7 @@ use crate::{
 	error_code::AUTH_VERIFICATION_FAILED_CODE, methods::PROTECTED_METHODS,
 	middlewares::HttpExtensions, verify_auth::verify_auth_token_authentication,
 };
-use heima_authentication::constants::AUTH_TOKEN_ID_TYPE;
+use heima_authentication::constants::{AUTH_TOKEN_ACCESS_TYPE, AUTH_TOKEN_ID_TYPE};
 use jsonrpsee::{
 	server::{
 		middleware::rpc::{ResponseFuture, RpcServiceT},
@@ -13,7 +13,6 @@ use jsonrpsee::{
 use tower::layer::util::{Identity, Stack};
 
 #[derive(Clone, Debug)]
-#[allow(dead_code)] // TODO: remove this once the extensions are used in the methods
 pub struct RpcExtensions {
 	pub sender: String,
 }
@@ -61,7 +60,7 @@ where
 				match verify_auth_token_authentication(
 					&self.rsa_private_key,
 					token,
-					AUTH_TOKEN_ID_TYPE, // TODO: conditionally set token type based on the method
+					auth_token_type_for_method(req.method_name()),
 					false,
 				) {
 					Ok(claims) => {
@@ -89,5 +88,17 @@ where
 		}
 
 		ResponseFuture::future(self.service.call(req))
+	}
+}
+
+// Defines the methods that require "access" auth token type
+const ACCESS_TOKEN_PROTECTED_METHODS: [&str; 2] =
+	["omni_notifyLimitOrderResult", "omni_signLimitOrder"];
+
+fn auth_token_type_for_method(method: &str) -> &'static str {
+	if ACCESS_TOKEN_PROTECTED_METHODS.contains(&method) {
+		AUTH_TOKEN_ACCESS_TYPE
+	} else {
+		AUTH_TOKEN_ID_TYPE
 	}
 }
