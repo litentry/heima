9807711c2ec67cd47c26b4ae32ef0234c4bf35c1
BillyWooo <yang@trustcomputing.de>
2025-06-13 14:06:05 +0000

    fmt

--- parachain/runtime/heima/src/migration/mod.rs
+++ parachain/runtime/heima/src/migration/mod.rs
@@ -17,11 +17,11 @@
 #[cfg(feature = "try-runtime")]
 use frame_support::ensure;
 #[cfg(feature = "try-runtime")]
-use sp_std::vec::Vec;
-#[cfg(feature = "try-runtime")]
 use frame_support::traits::StorageVersion;
 #[cfg(feature = "try-runtime")]
 use sp_runtime::DispatchError;
+#[cfg(feature = "try-runtime")]
+use sp_std::vec::Vec;
 
 use frame_support::{
 	pallet_prelude::*,
