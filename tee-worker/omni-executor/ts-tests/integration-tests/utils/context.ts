3fd131b41fe44ffbccafcfab183230bc5f35167b
Francisco Silva <franjs.francisco@gmail.com>
2025-06-13 08:58:36 +0200

    Enable omni-account integration tests and fix API package references (#3517)

--- tee-worker/omni-executor/ts-tests/integration-tests/utils/context.ts
+++ tee-worker/omni-executor/ts-tests/integration-tests/utils/context.ts
@@ -1,7 +1,7 @@
 import type { HexString } from '@polkadot/util/types';
 import { ApiPromise, WsProvider } from '@polkadot/api';
-import { omniAccount, omniExecutor } from '@heima-network/api-argument/omni';
-import { identity, trusted_operations} from '@heima-network/api-argument/identity';
+import { omniAccount, omniExecutor } from '@heima-network/api-augment/omni';
+import { identity, trusted_operations} from '@heima-network/api-augment/identity';
 import { hexToString } from '@polkadot/util';
 import WebSocketAsPromised from 'websocket-as-promised';
 import WsAsPromiseOptions from 'websocket-as-promised/types/options';
