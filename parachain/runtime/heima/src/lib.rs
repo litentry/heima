9e12d86430e06483f5d829e5a5c1abd6c38aace1
BillyWooo <yang@trustcomputing.de>
2025-06-13 13:06:13 +0000

    add migration logic for pallet-identity -> StorageVersion, and pallet-xcm -> V2 to V5

--- parachain/runtime/heima/src/lib.rs
+++ parachain/runtime/heima/src/lib.rs
@@ -105,6 +105,7 @@ pub mod asset_config;
 pub mod constants;
 pub mod precompiles;
 
+pub mod migration;
 pub mod weights;
 pub mod xcm_config;
 
@@ -163,6 +164,7 @@ pub type Executive = frame_executive::Executive<
 	// It was reverse order before.
 	// See the comment before collation related pallets too.
 	AllPalletsWithSystem,
+	migration::Migrations<Runtime>,
 >;
 
 impl fp_self_contained::SelfContainedCall for RuntimeCall {
