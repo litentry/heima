f46dc5f7386c113450f921db515576e1704bf611
BillyWooo <yang@trustcomputing.de>
2025-06-24 21:09:17 +0000

    stable2412 should use version number 0.9.25

--- parachain/runtime/paseo/src/lib.rs
+++ parachain/runtime/paseo/src/lib.rs
@@ -248,7 +248,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_name: alloc::borrow::Cow::Borrowed("heima"),
 	authoring_version: 1,
 	// same versioning-mechanism as polkadot: use last digit for minor updates
-	spec_version: 9244,
+	spec_version: 9252,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
