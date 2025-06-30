93531031214e46adc98dd8cd489067c8cbe9c6ae
Faisal Ahmed <faisalahmedfarooq46@gmail.com>
2025-07-01 04:59:54 +0530

    refactor: add license headers to all files

--- tee-worker/omni-executor/evm-aggregators-client/src/evm_tx_manager.rs
+++ tee-worker/omni-executor/evm-aggregators-client/src/evm_tx_manager.rs
@@ -1,3 +1,19 @@
+// Copyright 2020-2025 Trust Computing GmbH.
+// This file is part of Litentry.
+//
+// Litentry is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Litentry is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Litentry.  If not, see <https://www.gnu.org/licenses/>.
+
 use crate::common::{
 	is_native_token, CreateMarketTx, CROSS_SERVICE_FEE_BPS, CROSS_SERVICE_FEE_PERCENT,
 	DECIMALS_TO_VALUE, INCH_DEX_IDS_MAP, INCH_SWAP_APPROVE_ADDRESS, KYBER_SWAP_APPROVE_ADDRESS,
--- tee-worker/omni-executor/evm-aggregators-client/src/inch_client/client.rs
+++ tee-worker/omni-executor/evm-aggregators-client/src/inch_client/client.rs
@@ -1,5 +1,19 @@
-// This is where the client code of 1inch will reside
-// It's main object will be to provide swapping
+// Copyright 2020-2025 Trust Computing GmbH.
+// This file is part of Litentry.
+//
+// Litentry is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Litentry is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Litentry.  If not, see <https://www.gnu.org/licenses/>.
+
 use crate::inch_client::types::{SwapRequest, SwapResponse};
 use async_trait::async_trait;
 use log::error;
