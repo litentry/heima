bcb4c105178023e3233865c3d28c431dc29f7f1e
Kailai-Wang <Kailai.Wang@hotmail.com>
2025-07-02 14:23:24 +0000

    init

---
+++ tee-worker/omni-executor/rpc-server/src/methods/omni/get_smart_wallet_root_signer.rs
@@ -0,0 +1,63 @@
+use super::common::{check_omni_api_response, handle_omni_native_task};
+use crate::{
+	error_code::*,
+	methods::omni::{common::check_auth, PumpxRpcError},
+	server::RpcContext,
+	ErrorCode,
+};
+use executor_core::native_task::*;
+use executor_primitives::{
+	utils::hex::{hex_encode, FromHexPrefixed},
+	AccountId,
+};
+use heima_primitives::Address32;
+use jsonrpsee::RpcModule;
+use native_task_handler::NativeTaskOk;
+use pumpx::methods::add_wallet::AddWalletResponse;
+use serde::Serialize;
+use signer_client::ChainType;
+use tracing::{debug, error};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct GetSmartWalletRootSignerParams {
+	pub chain_type: ChainType,
+	pub index: u32,
+}
+
+pub fn register_get_smart_wallet_root_signer(module: &mut RpcModule<RpcContext>) {
+	module
+		.register_async_method("omni_getSmartWalletRootSigner", |params, ctx, ext| async move {
+			let user = check_auth(&ext).map_err(|e| {
+				error!("Authentication check failed: {:?}", e);
+				PumpxRpcError::from_error_code(ErrorCode::ServerError(
+					AUTH_VERIFICATION_FAILED_CODE,
+				))
+			})?;
+
+			let params = params.parse::<GetSmartWalletRootSignerParams>().map_err(|e| {
+				error!("Failed to parse params: {:?}", e);
+				PumpxRpcError::from_error_code(ErrorCode::ParseError)
+			})?;
+
+			debug!("Received omni_getSmartWalletRootSigner");
+
+			let Ok(address) = Address32::from_hex(&user.omni_account) else {
+				error!("Failed to parse from omni account token");
+				return Err(PumpxRpcError::from_error_code(ErrorCode::InternalError));
+			};
+
+			let wallet = ctx
+				.signer_client
+				.request_wallet(params.chain_type, params.index, address.into())
+				.await
+				.map_err(|_| {
+					error!("Failed to request wallet from signer client");
+					PumpxRpcError::from_error_code(ErrorCode::ServerError(
+						PUMPX_SIGNER_REQUEST_WALLET_FAILED_CODE,
+					))
+				})?;
+
+			Ok::<String, ErrorObject>(hex_encode(&wallet))
+		})
+		.expect("Failed to register omni_addWallet method");
+}
