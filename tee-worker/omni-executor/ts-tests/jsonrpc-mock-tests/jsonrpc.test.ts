6f3a482dabb24340a8742c8e9f3a820b80082640
0xverin <104152026+0xverin@users.noreply.github.com>
2025-07-04 17:33:12 +0800

    Add a template for JSON-RPC mock tests. (#3577)
    * add requestEmailVerificationCode case
    
    * Added JSON-RPC mock test scripts and related Docker configurations
    
    * update README
    
    * revert docke compose
    
    * update lock file
    
    * update pnpm-lock.yaml
    
    * add todo comment
    
    * fmt
    
    * update environment
    
    * chmod file
    
    * update volumes
    
    * rename project
    
    * update  docker compose
    
    * revert docker-compose.test.yml
    
    * enable mock-server
    
    * using OMNI_WORKER_ENDPOINT as rpc endpoint
    
    * update ci workflow, always rebuild omni executor while need to run omni executor test
    
    * add mock api
    
    * rm unused code
    
    ---------
    
    Co-authored-by: wli-pro <wli-pro>
    Co-authored-by: BillyWooo <yang@trustcomputing.de>

---
+++ tee-worker/omni-executor/ts-tests/jsonrpc-mock-tests/jsonrpc.test.ts
@@ -0,0 +1,22 @@
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+import { omniApi, RequestEmailVerificationCodeResponse } from './utils';
+
+enum ClientId {
+    Wildmeta = 'wildmeta',
+    Heima = 'heima',
+    Pumpx = 'pumpx',
+}
+
+describe('Request Email Verification Code', () => {
+    it('should request email verification code successfully', async function () {
+        this.timeout(10000);
+
+        const result: RequestEmailVerificationCodeResponse = await omniApi.requestEmailVerificationCode({
+            client_id: ClientId.Wildmeta,
+            user_email: 'test@gmail.com',
+        });
+
+        expect(result).to.be.null;
+    });
+});
