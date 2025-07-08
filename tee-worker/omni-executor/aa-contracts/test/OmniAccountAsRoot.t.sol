3be0be95629759c738edd4116760f94321723f77
Kailai-Wang <Kailai.Wang@hotmail.com>
2025-07-08 08:03:39 +0000

    add test

--- tee-worker/omni-executor/aa-contracts/test/OmniAccountAsRoot.t.sol
+++ tee-worker/omni-executor/aa-contracts/test/OmniAccountAsRoot.t.sol
@@ -51,6 +51,24 @@ contract OmniAccountAsRoot is Test {
         assertEq(SIG_VALIDATION_SUCCESS, validationData);
     }
 
+    function test_validateOpFailsWithWrongSigType() public {
+        (address root, uint256 rootPk) = makeAddrAndKey("root");
+        (counter, entryPoint, account) = OmniAccountTestUtils.setUp(ownerAddress, clientId, root);
+
+        address sender = 0x0eAfeE130Ab1F6261885eE7080f9e8B2513111d4;
+        bytes memory initCode = "";
+
+        PackedUserOperation memory packedOp = TestUtils.preparePackedOp(sender, initCode);
+        bytes32 packedOpHash = entryPoint.getUserOpHash(packedOp);
+        // sign userOp
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(rootPk, packedOpHash);
+        packedOp.signature = abi.encodePacked(uint8(UserOpSigType.EOA), r, s, v); // The SigType is wrong here
+
+        vm.prank(address(entryPoint));
+        uint256 validationData = account.validateUserOp(packedOp, packedOpHash, 0);
+        assertEq(SIG_VALIDATION_FAILED, validationData);
+    }
+
     function test_validateOpSessionKey() public {
         (address root, uint256 rootPk) = makeAddrAndKey("root");
         (address session, uint256 sessionPk) = makeAddrAndKey("session");
