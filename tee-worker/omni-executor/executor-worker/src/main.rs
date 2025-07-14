ba4ced8fa03ea0cf3ab4791d998cf76d00ca0d69
Francisco Silva <franjs.francisco@gmail.com>
2025-07-14 18:59:05 +0200

    feat(executor): use WildmetaApiClient when API URL is configured

--- tee-worker/omni-executor/executor-worker/src/main.rs
+++ tee-worker/omni-executor/executor-worker/src/main.rs
@@ -441,7 +441,15 @@ async fn main() -> Result<(), ()> {
 
 			// Create wildmeta API client and timestamp storage
 			let wildmeta_api: Arc<Box<dyn wildmeta_api::WildmetaApi>> =
-				Arc::new(Box::new(wildmeta_api::MockWildmetaApi));
+				if let Some(url) = &config_loader.wildmeta_api_url {
+					if url.is_empty() {
+						Arc::new(Box::new(wildmeta_api::MockWildmetaApi))
+					} else {
+						Arc::new(Box::new(wildmeta_api::WildmetaApiClient::new(url.clone())))
+					}
+				} else {
+					Arc::new(Box::new(wildmeta_api::MockWildmetaApi))
+				};
 			let wildmeta_timestamp_storage = Arc::new(
 				executor_storage::WildmetaTimestampStorage::new(storage_db.clone())
 			);
