598274e999a7e9a132513bc91dd1f3d6f8179797
0xverin <daqingchong0809@gmail.com>
2025-07-25 04:34:14 +0000

    Revise the ESLint configuration

---
+++ tee-worker/identity/ts-tests/worker/eslint.config.js
@@ -0,0 +1,54 @@
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+    js.configs.recommended,
+    {
+        files: ['**/*.ts', '**/*.tsx'],
+        languageOptions: {
+            parser: tsparser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tseslint,
+        },
+        rules: {
+            /**
+        It's a temporary solution, folks. We had no choice but to shut it off,
+        because there's just a liiittle bit too much "any" lurking around in the code.
+        But fear not, my friends, for this is not the end of the story.
+        We shall return, armed with determination and resolve,
+        to tackle those "any" types head-on in the near future.
+        **/
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-non-null-assertion': 'off',
+            '@typescript-eslint/no-var-requires': 'off',
+
+            // explanation: https://typescript-eslint.io/rules/naming-convention/
+            '@typescript-eslint/naming-convention': [
+                'error',
+                {
+                    selector: 'typeLike',
+                    format: ['StrictPascalCase'],
+                },
+                {
+                    selector: 'variable',
+                    modifiers: ['const'],
+                    format: ['strictCamelCase', 'UPPER_CASE'],
+                },
+                {
+                    selector: 'function',
+                    format: ['strictCamelCase', 'StrictPascalCase'],
+                },
+                {
+                    selector: 'parameter',
+                    format: ['strictCamelCase'],
+                },
+            ],
+        },
+    },
+];
