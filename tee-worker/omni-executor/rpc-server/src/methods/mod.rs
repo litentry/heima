0e41068fc2b58deeff59b5de361697c92494438f
Francisco Silva <franjs.francisco@gmail.com>
2025-08-08 15:31:54 +0200

    feat(rpc): protect omni_estimateUserOpGas method
    Added "omni_estimateUserOpGas" to the PROTECTED_METHODS array in the RPC
    server. This ensures that the method is now access-controlled alongside
    other sensitive RPC methods.

--- tee-worker/omni-executor/rpc-server/src/methods/mod.rs
+++ tee-worker/omni-executor/rpc-server/src/methods/mod.rs
@@ -7,7 +7,7 @@ use pumpx::*;
 mod omni;
 use omni::*;
 
-pub const PROTECTED_METHODS: [&str; 8] = [
+pub const PROTECTED_METHODS: [&str; 9] = [
 	"omni_testProtectedMethod",
 	"omni_addWallet",
 	"omni_notifyLimitOrderResult",
@@ -16,6 +16,7 @@ pub const PROTECTED_METHODS: [&str; 8] = [
 	"omni_submitUserOp",
 	"omni_transferWithdraw",
 	"omni_exportWallet",
+	"omni_estimateUserOpGas",
 ];
 
 pub fn register_methods(module: &mut RpcModule<RpcContext>) {
