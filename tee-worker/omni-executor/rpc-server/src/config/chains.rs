784e5438d188740deab9ca3ad2b935755ed03204
Francisco Silva <franjs.francisco@gmail.com>
2025-08-14 16:14:09 +0200

    feat(rpc): update supported EVM chains
    - Add Sepolia, Arbitrum Sepolia, Optimism Sepolia testnets
    - Add Polygon Mumbai, Base Sepolia, Local Anvil
    - Remove deprecated Goerli testnets and BSC

--- tee-worker/omni-executor/rpc-server/src/config/chains.rs
+++ tee-worker/omni-executor/rpc-server/src/config/chains.rs
@@ -1,14 +1,16 @@
 // Supported EVM-compatible chains
 pub const SUPPORTED_EVM_CHAINS: &[u32] = &[
-	1,      // Ethereum Mainnet
-	5,      // Goerli
-	10,     // Optimism
-	56,     // BSC
-	137,    // Polygon
-	42161,  // Arbitrum One
-	421613, // Arbitrum Goerli
-	8453,   // Base
-	84531,  // Base Goerli
+	1,        // Ethereum Mainnet
+	11155111, // Sepolia
+	42161,    // Arbitrum
+	421614,   // Arbitrum Sepolia
+	10,       // Optimism
+	11155420, // Optimism Sepolia
+	137,      // Polygon
+	80001,    // Polygon Mumbai
+	8453,     // Base
+	84532,    // Base Sepolia
+	1337,     // Local Anvil
 ];
 
 // Maximum wallet index allowed
