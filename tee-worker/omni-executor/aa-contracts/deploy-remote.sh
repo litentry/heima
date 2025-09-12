94a275d0ad40d8493e74f8e7a89d2ed3eca9c96f
Kailai-Wang <Kailai.Wang@hotmail.com>
2025-09-12 16:37:36 +0000

    use --slow

--- tee-worker/omni-executor/aa-contracts/deploy-remote.sh
+++ tee-worker/omni-executor/aa-contracts/deploy-remote.sh
@@ -38,7 +38,7 @@ if [ -n "$ETHERSCAN_API_KEY" ]; then
       --broadcast \
       --verify \
       --etherscan-api-key $ETHERSCAN_API_KEY \
-      --confirmations 5 \
+      --slow \
       -vvv
 else
     echo "🚀 Running deployment without verification (no ETHERSCAN_API_KEY set)..."
@@ -46,7 +46,7 @@ else
       --rpc-url $RPC_URL \
       --private-key $PRIVATE_KEY \
       --broadcast \
-      --confirmations 5 \
+      --slow \
       -vvv
 fi
 
