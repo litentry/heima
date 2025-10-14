45e87e66ae524e724bfbcc910f781d9a9f6e429d
Kailai-Wang <Kailai.Wang@hotmail.com>
2025-10-14 15:17:58 +0000

    remove from protected method

--- tee-worker/omni-executor/rpc-server/src/methods/mod.rs
+++ tee-worker/omni-executor/rpc-server/src/methods/mod.rs
@@ -5,7 +5,7 @@ use jsonrpsee::RpcModule;
 mod omni;
 use omni::*;
 
-pub const PROTECTED_METHODS: [&str; 9] = [
+pub const PROTECTED_METHODS: [&str; 8] = [
 	"omni_testProtectedMethod",
 	"omni_addWallet",
 	"omni_notifyLimitOrderResult",
@@ -14,7 +14,6 @@ pub const PROTECTED_METHODS: [&str; 9] = [
 	"omni_submitUserOp",
 	"omni_transferWithdraw",
 	"omni_exportWallet",
-	"omni_requestLoan",
 ];
 
 pub fn register_methods<
